3b7489f2cb43921e6d5c4b7566caa9dc39b26da0
Reaver01 <reaver_01@gmx.com>
2016-07-21 17:47:52 +0200

    different logic

--- pokecli.py
+++ pokecli.py
@@ -152,9 +152,9 @@ def main():
 
     response_dict = api.call()
     #print('Response dictionary: \n\r{}'.format(json.dumps(response_dict, indent=2)))
-    if response_dict['responses']['GET_PLAYER']['profile']['currency'][0]['amount']:
+    if 'amount' in response_dict['responses']['GET_PLAYER']['profile']['currency'][0]:
         currency_1=response_dict['responses']['GET_PLAYER']['profile']['currency'][0]['amount']
-    if response_dict['responses']['GET_PLAYER']['profile']['currency'][1]['amount']:
+    if 'amount' in response_dict['responses']['GET_PLAYER']['profile']['currency'][1]:
         currency_2=response_dict['responses']['GET_PLAYER']['profile']['currency'][1]['amount']
     print('Profile: \n    Username: %s\n    Bag size: %s\n    Pokemon Storage Size: %s\n    Account Creation: %s\n    Currency:\n        %s: %s\n        %s: %s', response_dict['responses']['GET_PLAYER']['profile']['username'], response_dict['responses']['GET_PLAYER']['profile']['item_storage'], response_dict['responses']['GET_PLAYER']['profile']['poke_storage'], response_dict['responses']['GET_PLAYER']['profile']['creation_time'], response_dict['responses']['GET_PLAYER']['profile']['currency'][0]['type'], currency_1, response_dict['responses']['GET_PLAYER']['profile']['currency'][1]['type'], currency_2)
 
