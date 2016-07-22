7cf784ce5d1a7069b1e73cd68219c0e4de606932
Douglas Camata <d.camata@gmail.com>
2016-07-22 13:31:33 +0200

    added a missing i2f function and some imports to utils

--- cell_workers/utils.py
+++ cell_workers/utils.py
@@ -1,4 +1,11 @@
+import struct
+from math import cos, asin, sqrt
+
+
 def distance(lat1, lon1, lat2, lon2):
     p = 0.017453292519943295
     a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
     return 12742 * asin(sqrt(a)) * 1000
+
+def i2f(int):
+    return struct.unpack('<d', struct.pack('<Q', int))[0]
