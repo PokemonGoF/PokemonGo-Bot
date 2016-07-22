aad3c16e42f4a46b9ecc96c4f1c2253f7639fa3d
PokemonGoF <PokemonGoF@github.com>
2016-07-22 02:08:05 -0700

    To fix the useless move.

--- stepper.py
+++ stepper.py
@@ -29,20 +29,6 @@ class Stepper(object):
         for step in range(self.steplimit2):
             #starting at 0 index
             print('[#] Scanning area for objects ({} / {})'.format((step+1), self.steplimit**2))
-            if self.config.debug:
-                print('steplimit: {} x: {} y: {} pos: {} dx: {} dy {}'.format(self.steplimit2, self.x, self.y, self.pos, self.dx, self.dy))
-            # Scan location math
-            if -self.steplimit2 / 2 < self.x <= self.steplimit2 / 2 and -self.steplimit2 / 2 < self.y <= self.steplimit2 / 2:
-                position = (self.x * 0.0025 + self.origin_lat, self.y * 0.0025 + self.origin_lon, 0)
-                if self.config.walk > 0:
-                    self.api.walk(self.config.walk, *position,walking_hook=self.walking_hook)
-                else:
-                    self.api.set_position(*position)
-                print(position)
-            if self.x == self.y or self.x < 0 and self.x == -self.y or self.x > 0 and self.x == 1 - self.y:
-                (self.dx, self.dy) = (-self.dy, self.dx)
-
-            (self.x, self.y) = (self.x + self.dx, self.y + self.dy)
 
             # get map objects call
             # ----------------------
@@ -58,9 +44,24 @@ class Stepper(object):
                 response_dict['responses']['GET_MAP_OBJECTS']['status'] is 1:
                 #print('got the maps')
                 map_cells=response_dict['responses']['GET_MAP_OBJECTS']['map_cells']
-                #print('map_cells are {}'.format(len(map_cells)))
+                print('map_cells are {}'.format(len(map_cells)))
                 for cell in map_cells:
                     self.bot.work_on_cell(cell,position)
+
+            if self.config.debug:
+                print('steplimit: {} x: {} y: {} pos: {} dx: {} dy {}'.format(self.steplimit2, self.x, self.y, self.pos, self.dx, self.dy))
+            # Scan location math
+            if -self.steplimit2 / 2 < self.x <= self.steplimit2 / 2 and -self.steplimit2 / 2 < self.y <= self.steplimit2 / 2:
+                position = (self.x * 0.0025 + self.origin_lat, self.y * 0.0025 + self.origin_lon, 0)
+                if self.config.walk > 0:
+                    self.api.walk(self.config.walk, *position,walking_hook=self.walking_hook)
+                else:
+                    self.api.set_position(*position)
+                print(position)
+            if self.x == self.y or self.x < 0 and self.x == -self.y or self.x > 0 and self.x == 1 - self.y:
+                (self.dx, self.dy) = (-self.dy, self.dx)
+
+            (self.x, self.y) = (self.x + self.dx, self.y + self.dy)
             time.sleep(10)
 
     def _get_cellid(self, lat, long):
