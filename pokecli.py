fa8eb9036c2c23174dfc01a0ac50a8b734cd261c
Steve Birstok <steve.birstok@nbcuni.com>
2016-07-22 12:57:51 -0400

    code cleanup

--- pokecli.py
+++ pokecli.py
@@ -24,16 +24,14 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 Author: tjado <https://github.com/tejado>
 """
 
-import os
-import re
-import json
-import requests
-import argparse
-import time
-import ssl
+from argparse import ArgumentParser
+from json import load as read_json
+from os.path import isfile
+from time import sleep
+from codecs import getwriter
 import logging
+import ssl
 import sys
-import codecs
 
 if sys.version_info >= (2, 7, 9):
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -41,14 +39,14 @@ if sys.version_info >= (2, 7, 9):
 from bot import PokemonGoBot
 
 def init_config():
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     config_file = "config.json"
 
     # If config file exists, load variables from json
-    load   = {}
-    if os.path.isfile(config_file):
+    load = {}
+    if isfile(config_file):
         with open(config_file) as data:
-            load.update(json.load(data))
+            load.update(read_json(data))
 
     # Read passed in Arguments
     required = lambda x: not x in load
@@ -59,7 +57,7 @@ def init_config():
     parser.add_argument("-l", "--location", help="Location", required=required("location"))
     parser.add_argument("-s", "--spinstop", help="SpinPokeStop", action='store_true')
     parser.add_argument("-w", "--walk", help="Walk instead of teleport with given speed (meters per second, e.g. 2.5)", type=float, default=2.5)
-    parser.add_argument("-c", "--cp",help="Set CP less than to transfer(DEFAULT 100)",type=int,default=100)
+    parser.add_argument("-c", "--cp", help="Set CP less than to transfer(DEFAULT 100)", type=int, default=100)
     parser.add_argument("-k", "--gmapkey",help="Set Google Maps API KEY",type=str,default=None)
     parser.add_argument("--maxsteps",help="Set the steps around your initial location(DEFAULT 5 mean 25 cells around your location)",type=int,default=5)
     parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
@@ -69,12 +67,12 @@ def init_config():
 
     # Passed in arguments shoud trump
     for key in config.__dict__:
-        if key in load and config.__dict__[key] == None:
+        if key in load and config.__dict__[key] is None:
             config.__dict__[key] = load[key]
 
     if config.auth_service not in ['ptc', 'google']:
-      log.error("Invalid Auth service specified! ('ptc' or 'google')")
-      return None
+        logging.error("Invalid Auth service ('%s') specified! ('ptc' or 'google')", config.auth_service)
+        return None
 
     return config
 
@@ -83,13 +81,13 @@ def main():
     # log format
     #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
 
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-    sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+    sys.stdout = getwriter('utf8')(sys.stdout)
+    sys.stderr = getwriter('utf8')(sys.stderr)
 
     # @eggins clean log
-    print('[x] Initializing PokemonGO Bot v1.0')
-    time.sleep(1)
-    print('[x] PokemonGo Bot [@PokemonGoF | @eggins | @crack00r | @ethervoid | /r/pokemongodev]')
+    print '[x] Initializing PokemonGO Bot v1.0'
+    sleep(1)
+    print '[x] PokemonGo Bot [@PokemonGoF | @eggins | @crack00r | @ethervoid | /r/pokemongodev]'
 
     config = init_config()
     if not config:
@@ -108,13 +106,16 @@ def main():
         logging.getLogger("pgoapi").setLevel(logging.DEBUG)
         logging.getLogger("rpc_api").setLevel(logging.DEBUG)
 
-    print('[x] Configuration Initialized')
+    print '[x] Configuration Initialized'
 
-    bot = PokemonGoBot(config)
-    bot.start()
+    try:
+        bot = PokemonGoBot(config)
+        bot.start()
 
-    while(True):
-        bot.take_step()
+        while True:
+            bot.take_step()
+    except KeyboardInterrupt:
+        print '[ USER ABORTED, EXITING.. ]'
 
 if __name__ == '__main__':
     main()
