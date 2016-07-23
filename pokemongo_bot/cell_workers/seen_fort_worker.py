e40f17c5edf60757b2948cc12c5e7f09d2956707
James Droste <james@droste.im>
2016-07-23 14:02:35 +0100

    Ran autopep8 on our code

--- pokemongo_bot/cell_workers/seen_fort_worker.py
+++ pokemongo_bot/cell_workers/seen_fort_worker.py
@@ -22,13 +22,14 @@ class SeenFortWorker(object):
     def work(self):
         lat = self.fort['latitude']
         lng = self.fort['longitude']
-        unit = self.config.distance_unit # Unit to use when printing formatted distance
+        unit = self.config.distance_unit  # Unit to use when printing formatted distance
 
         fortID = self.fort['id']
         dist = distance(self.position[0], self.position[1], lat, lng)
 
-        #print('[#] Found fort {} at distance {}m'.format(fortID, dist))
-        print('[#] Found fort {} at distance {}'.format(fortID, format_dist(dist, unit)))
+        # print('[#] Found fort {} at distance {}m'.format(fortID, dist))
+        print('[#] Found fort {} at distance {}'.format(
+            fortID, format_dist(dist, unit)))
 
         if dist > 10:
             print('[#] Need to move closer to Pokestop')
@@ -38,31 +39,34 @@ class SeenFortWorker(object):
                 self.stepper._walk_to(self.config.walk, *position)
             else:
                 self.api.set_position(*position)
-            self.api.player_update(latitude=lat,longitude=lng)
+            self.api.player_update(latitude=lat, longitude=lng)
             response_dict = self.api.call()
             print('[#] Arrived at Pokestop')
             sleep(2)
 
-        self.api.fort_details(fort_id=self.fort['id'], latitude=lat, longitude=lng)
+        self.api.fort_details(
+            fort_id=self.fort['id'], latitude=lat, longitude=lng)
         response_dict = self.api.call()
         if 'responses' in response_dict \
-            and'FORT_DETAILS' in response_dict['responses'] \
-            and 'name' in response_dict['responses']['FORT_DETAILS']:
+                and'FORT_DETAILS' in response_dict['responses'] \
+                and 'name' in response_dict['responses']['FORT_DETAILS']:
             fort_details = response_dict['responses']['FORT_DETAILS']
             fort_name = fort_details['name'].encode('utf8', 'replace')
         else:
             fort_name = 'Unknown'
         print_yellow('[#] Now at Pokestop: ' + fort_name + ' - Spinning...')
         sleep(2)
-        self.api.fort_search(fort_id=self.fort['id'], fort_latitude=lat, fort_longitude=lng, player_latitude=f2i(self.position[0]), player_longitude=f2i(self.position[1]))
+        self.api.fort_search(fort_id=self.fort['id'], fort_latitude=lat, fort_longitude=lng, player_latitude=f2i(
+            self.position[0]), player_longitude=f2i(self.position[1]))
         response_dict = self.api.call()
         if 'responses' in response_dict and \
-            'FORT_SEARCH' in response_dict['responses']:
+                'FORT_SEARCH' in response_dict['responses']:
 
             spin_details = response_dict['responses']['FORT_SEARCH']
             if spin_details['result'] == 1:
                 print_green("[+] Loot: ")
-                experience_awarded = spin_details.get('experience_awarded', False)
+                experience_awarded = spin_details.get(
+                    'experience_awarded', False)
                 if experience_awarded:
                     print_green("[+] " + str(experience_awarded) + " xp")
 
@@ -80,15 +84,18 @@ class SeenFortWorker(object):
                         item_id = str(item_id)
                         item_name = self.item_list[item_id]
 
-                        print_green("[+] " + str(item_count) + "x " + item_name)
+                        print_green("[+] " + str(item_count) +
+                                    "x " + item_name)
 
                 else:
                     print_yellow("[#] Nothing found.")
 
-                pokestop_cooldown = spin_details.get('cooldown_complete_timestamp_ms')
+                pokestop_cooldown = spin_details.get(
+                    'cooldown_complete_timestamp_ms')
                 if pokestop_cooldown:
                     seconds_since_epoch = time.time()
-                    print('[#] PokeStop on cooldown. Time left: ' + str(format_time((pokestop_cooldown/1000) - seconds_since_epoch)))
+                    print('[#] PokeStop on cooldown. Time left: ' +
+                          str(format_time((pokestop_cooldown / 1000) - seconds_since_epoch)))
 
                 if not items_awarded and not experience_awarded and not pokestop_cooldown:
                     message = (
@@ -103,14 +110,15 @@ class SeenFortWorker(object):
             elif spin_details['result'] == 2:
                 print("[#] Pokestop out of range")
             elif spin_details['result'] == 3:
-                pokestop_cooldown = spin_details.get('cooldown_complete_timestamp_ms')
+                pokestop_cooldown = spin_details.get(
+                    'cooldown_complete_timestamp_ms')
                 if pokestop_cooldown:
                     seconds_since_epoch = time.time()
                     print('[#] PokeStop on cooldown. Time left: ' + str(
                         format_time((pokestop_cooldown / 1000) - seconds_since_epoch)))
             elif spin_details['result'] == 4:
                 print_red("[#] Inventory is full, switching to catch mode...")
-                self.config.mode='poke'
+                self.config.mode = 'poke'
 
             if 'chain_hack_sequence_number' in response_dict['responses']['FORT_SEARCH']:
                 time.sleep(2)
