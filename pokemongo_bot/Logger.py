e99c35345e71161e3585f222898284e9baace4f2
brantje <brantje@gmail.com>
2016-07-23 16:09:58 +0200

    Use a dict instead of a big if

--- pokemongo_bot/Logger.py
+++ pokemongo_bot/Logger.py
@@ -7,17 +7,16 @@ except:
     lcd = False
 
 def log(string, color = 'white'):
-    colorHex = ''
-    if(color == 'green'):
-        colorHex = '92m'
-    if(color == 'yellow'):
-        colorHex = '93m'
-    if(colorHex == 'red'):
-        colorHex = '91m'
-    if colorHex == '':
+    colorHex = {
+        'green': '92m',
+        'yellow': '93m',
+        'red': '91m'
+    }
+
+    if color not in colorHex:
         print(string)
     else:
-        print(u'\033['+ colorHex + string.decode('utf-8') + '\033[0m')
+        print(u'\033['+ colorHex[color] + string.decode('utf-8') + '\033[0m')
     if lcd:
         if(string):
             lcd.message(string)
