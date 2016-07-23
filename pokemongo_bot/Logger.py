6b167ed34f6dc04505f4d54447ef7324b7e3f014
brantje <brantje@gmail.com>
2016-07-23 15:35:26 +0200

    Add LCD support

--- pokemongo_bot/Logger.py
+++ pokemongo_bot/Logger.py
@@ -1,4 +1,10 @@
-
+try:
+    import lcd
+    lcd = lcd.lcd()
+    # Change this to your i2c address
+    lcd.set_addr(0x23)
+except:
+    lcd = False
 
 def log(string, color = 'white'):
     colorHex = ''
@@ -12,3 +18,6 @@ def log(string, color = 'white'):
         print(string)
     else:
         print(u'\033['+ colorHex + string.decode('utf-8') + '\033[0m')
+    if lcd:
+        if(string):
+            lcd.message(string)
