fe2686d617174b58646a4bdff2da2f0f78bcbd7e
Christopher Story <kc5hhq@gmail.com>
2016-07-23 13:24:03 -0500

    Fix formatting of date/time in logger.

--- pokemongo_bot/logger.py
+++ pokemongo_bot/logger.py
@@ -14,11 +14,9 @@ def log(string, color = 'white'):
         'red': '91m'
     }
     if color not in colorHex:
-        print(time.strftime("%Y-%m-%d %H:%M:%S"))
-        print(string)
+        print('[' + time.strftime("%Y-%m-%d %H:%M:%S") + '] '+ string)
     else:
-        print(u'\033['+ colorHex[color] + time.strftime("%Y-%m-%d %H:%M:%S") + '\033[0m')
-        print(u'\033['+ colorHex[color] + string.decode('utf-8') + '\033[0m')
+        print(u'\033['+ colorHex[color] + time.strftime("%Y-%m-%d %H:%M:%S") + ' ' + string.decode('utf-8') + '\033[0m')
     if lcd:
         if(string):
             lcd.message(string)
