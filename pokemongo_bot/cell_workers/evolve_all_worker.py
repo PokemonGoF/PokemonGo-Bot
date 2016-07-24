5834c07d2236780215584dad090a3cbb8c11e959
Guy Zyskind <guy@zyskind.com>
2016-07-24 15:13:55 -0400

    longer sleeps - to avoid flooding the servers

--- pokemongo_bot/cell_workers/evolve_all_worker.py
+++ pokemongo_bot/cell_workers/evolve_all_worker.py
@@ -117,7 +117,7 @@ class EvolveAllWorker(object):
         else:
             # cache pokemons we can't evolve. Less server calls
             cache[pokemon_name] = 1
-        sleep(1.2)
+        sleep(5.7)
 
     # TODO: move to utils. These methods are shared with other workers.
     def transfer_pokemon(self, pid):
