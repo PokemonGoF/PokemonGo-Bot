9083237c5ae6b1ce1c78a19e8afc3be9600f3b90
Glenn <glenn@calcyfer.be>
2016-07-24 16:24:28 +0200

    Map UI improvements
    Better responsiveness
    Items in bag layout improved

--- web/main.js
+++ web/main.js
@@ -18,51 +18,51 @@ var menu;
 var stats = {};
 var teams = ["TeamLess", "Mystic", "Valor", "Instinct"];
 var out;
-var eggs = 0;
 var out1;
+var eggs = 0;
 var outArray = [];
 var itemsArray = {
-    "0": "Unknown",
+  "0": "Unknown",
 
-    "1": "Pokeball",
-    "2": "Greatball",
-    "3": "Ultraball",
-    "4": "Masterball",
+  "1": "Pokeball",
+  "2": "Greatball",
+  "3": "Ultraball",
+  "4": "Masterball",
 
-    "101": "Potion",
-    "102": "Super Potion",
-    "103": "Hyper Potion",
-    "104": "Max Potion",
+  "101": "Potion",
+  "102": "Super Potion",
+  "103": "Hyper Potion",
+  "104": "Max Potion",
 
-    "201": "Revive",
-    "202": "Max Revive",
+  "201": "Revive",
+  "202": "Max Revive",
 
-    "301": "Lucky Egg",
+  "301": "Lucky Egg",
 
-    "401": "Incense",
-    "402": "Spicy Incense",
-    "403": "Cool Incense",
-    "404": "Floral Incense",
+  "401": "Incense",
+  "402": "Spicy Incense",
+  "403": "Cool Incense",
+  "404": "Floral Incense",
 
-    "501": "Troy Disk",
+  "501": "Troy Disk",
 
-    "602": "X Attack",
-    "603": "X Defense",
-    "604": "X Miracle",
+  "602": "X Attack",
+  "603": "X Defense",
+  "604": "X Miracle",
 
-    "701": "Razz Berry",
-    "702": "Bluk Berry",
-    "703": "Nanab Berry",
-    "704": "Wepar Berry",
-    "705": "Pinap Berry",
+  "701": "Razz Berry",
+  "702": "Bluk Berry",
+  "703": "Nanab Berry",
+  "704": "Wepar Berry",
+  "705": "Pinap Berry",
 
-    "801": "Special Camera",
+  "801": "Special Camera",
 
-    "901": "Incubator (Unlimited)",
-    "902": "Incubator",
+  "901": "Incubator (Unlimited)",
+  "902": "Incubator",
 
-    "1001": "Pokemon Storage Upgrade",
-    "1002": "Item Storage Upgrade"
+  "1001": "Pokemon Storage Upgrade",
+  "1002": "Item Storage Upgrade"
 }
 
 
@@ -96,59 +96,59 @@ function initMap() {
 };
 
 $('#switchPan').change(function(){
-    if (this.checked) { userFollow = true } else { userFollow = false; }
+  if (this.checked) { userFollow = true } else { userFollow = false; }
 });
 
 $('#switchZoom').change(function(){
-    if (this.checked) { userZoom = true } else { userZoom = false; }
+  if (this.checked) { userZoom = true } else { userZoom = false; }
 });
 
 $('#imageType').change(function(){
-    if (this.checked) { imageExt = ".gif" } else { imageExt = ".png"; }
+  if (this.checked) { imageExt = ".gif" } else { imageExt = ".png"; }
 });
 
 $('#tInfo').click(function(){
-    if (menu == undefined || menu == 1) {
-      $("#submenu").toggle();
-    }
-    if (menu != 1 && $("#submenu").is(':hidden')) {
-      $("#submenu").toggle();
-    }
-    menu = 1;
-    buildMenu();
+  if (menu == undefined || menu == 1) {
+    $("#submenu").toggle();
+  }
+  if (menu != 1 && $("#submenu").is(':hidden')) {
+    $("#submenu").toggle();
+  }
+  menu = 1;
+  buildMenu();
 });
 
 $('#tItems').click(function(){
-    if (menu == undefined || menu == 2) {
-      $("#submenu").toggle();
-    }
-    if (menu != 2 && $("#submenu").is(':hidden')) {
-      $("#submenu").toggle();
-    }
-    menu = 2;
-    buildMenu();
+  if (menu == undefined || menu == 2) {
+    $("#submenu").toggle();
+  }
+  if (menu != 2 && $("#submenu").is(':hidden')) {
+    $("#submenu").toggle();
+  }
+  menu = 2;
+  buildMenu();
 });
 
 $('#tPokemon').click(function(){
-    if (menu == undefined || menu == 3) {
-      $("#submenu").toggle();
-    }
-    if (menu != 3 && $("#submenu").is(':hidden')) {
-      $("#submenu").toggle();
-    }
-    menu = 3;
-    buildMenu();
+  if (menu == undefined || menu == 3) {
+    $("#submenu").toggle();
+  }
+  if (menu != 3 && $("#submenu").is(':hidden')) {
+    $("#submenu").toggle();
+  }
+  menu = 3;
+  buildMenu();
 });
 
 $('#tPokedex').click(function(){
-    if (menu == undefined || menu == 4) {
-      $("#submenu").toggle();
-    }
-    if (menu != 4 && $("#submenu").is(':hidden')) {
-      $("#submenu").toggle();
-    }
-    menu = 4;
-    buildMenu();
+  if (menu == undefined || menu == 4) {
+    $("#submenu").toggle();
+  }
+  if (menu != 4 && $("#submenu").is(':hidden')) {
+    $("#submenu").toggle();
+  }
+  menu = 4;
+  buildMenu();
 });
 
 
@@ -266,15 +266,15 @@ var catchSuccess = function(data, user_index) {
           zIndex: 4,
           optimized: false
         });
-          if (userZoom == true) {
-            map.setZoom(16);
-          }
-          if (userFollow == true) {
-            map.panTo({
-              lat: parseFloat(data.latitude),
-              lng: parseFloat(data.longitude)
-            });
-          }
+        if (userZoom == true) {
+          map.setZoom(16);
+        }
+        if (userFollow == true) {
+          map.panTo({
+            lat: parseFloat(data.latitude),
+            lng: parseFloat(data.longitude)
+          });
+        }
       } else {
         user_data[users[user_index]].catchables[data.spawnpoint_id].setPosition({
           lat: parseFloat(data.latitude),
@@ -308,7 +308,7 @@ function addInventory() {
 function pad_with_zeroes(number, length) {
   var my_string = '' + number;
   while (my_string.length < length) {
-      my_string = '0' + my_string;
+    my_string = '0' + my_string;
   }
   return my_string;
 }
@@ -330,12 +330,12 @@ function loadJSON(path, success, error, successData) {
           success(JSON.parse(xhr.responseText.replace(/\bNaN\b/g, "null")), successData);
       } else {
         if (error)
-        error(xhr);
+          error(xhr);
       }
     }
   };
-xhr.open("GET", path, true);
-xhr.send();
+  xhr.open("GET", path, true);
+  xhr.send();
 }
 
 $(document).ready(function(){
@@ -349,28 +349,39 @@ function buildMenu() {
     out = '';
     for (var i = 0; i < stats.length; i++) {
       out += '<div class="row"><div class="col s12"><h5>' + users[0] + '</h5><br>Level: ' + stats[i].inventory_item_data.player_stats.level +
-      '<br>Exp: ' + stats[i].inventory_item_data.player_stats.experience +
-      '<br>Exp to Lvl ' + (parseInt(stats[i].inventory_item_data.player_stats.level) + 1) + ': ' + (parseInt(stats[i].inventory_item_data.player_stats.next_level_xp) - stats[i].inventory_item_data.player_stats.experience) +
-      '<br>Pokemon Encountered: ' + stats[i].inventory_item_data.player_stats.pokemons_encountered +
-      '<br>Pokeballs Thrown: ' + stats[i].inventory_item_data.player_stats.pokeballs_thrown +
-      '<br>Pokemon Caught: ' + stats[i].inventory_item_data.player_stats.pokemons_captured +
-      '<br>Small Ratata Caught: ' + stats[i].inventory_item_data.player_stats.small_rattata_caught +
-      '<br>Pokemon Evolved: ' + stats[i].inventory_item_data.player_stats.evolutions +
-      '<br>Eggs Hatched: ' + stats[i].inventory_item_data.player_stats.eggs_hatched +
-      '<br>Unique Pokedex Entries: ' + stats[i].inventory_item_data.player_stats.unique_pokedex_entries +
-      '<br>PokeStops Visited: ' + stats[i].inventory_item_data.player_stats.poke_stop_visits +
-      '<br>Kilometers Walked: ' + parseFloat(stats[i].inventory_item_data.player_stats.km_walked).toFixed(2) + '</div></div>';
+          '<br>Exp: ' + stats[i].inventory_item_data.player_stats.experience +
+          '<br>Exp to Lvl ' + (parseInt(stats[i].inventory_item_data.player_stats.level) + 1) + ': ' + (parseInt(stats[i].inventory_item_data.player_stats.next_level_xp) - stats[i].inventory_item_data.player_stats.experience) +
+          '<br>Pokemon Encountered: ' + stats[i].inventory_item_data.player_stats.pokemons_encountered +
+          '<br>Pokeballs Thrown: ' + stats[i].inventory_item_data.player_stats.pokeballs_thrown +
+          '<br>Pokemon Caught: ' + stats[i].inventory_item_data.player_stats.pokemons_captured +
+          '<br>Small Ratata Caught: ' + stats[i].inventory_item_data.player_stats.small_rattata_caught +
+          '<br>Pokemon Evolved: ' + stats[i].inventory_item_data.player_stats.evolutions +
+          '<br>Eggs Hatched: ' + stats[i].inventory_item_data.player_stats.eggs_hatched +
+          '<br>Unique Pokedex Entries: ' + stats[i].inventory_item_data.player_stats.unique_pokedex_entries +
+          '<br>PokeStops Visited: ' + stats[i].inventory_item_data.player_stats.poke_stop_visits +
+          '<br>Kilometers Walked: ' + parseFloat(stats[i].inventory_item_data.player_stats.km_walked).toFixed(2) + '</div></div>';
     }
     document.getElementById('subcontent').innerHTML = out;
   }
   if (menu == 2) {
     document.getElementById('subtitle').innerHTML = "Items in Bag";
-    out = '<div class="row items"><div class="col s12"><h5>' + users[0] + '</h5><table>';
+    out = '<div class="row items"><div class="col s12"><h5>' + users[0] + '</h5>';
     for (var i = 0; i < bagItems.length; i++) {
-      out += '<tr><td><img src="image/items/' + bagItems[i].inventory_item_data.item.item_id + '.png" class="item_img"></td><td>Item: ' + itemsArray[bagItems[i].inventory_item_data.item.item_id] +
-          '<br>Count: ' + (bagItems[i].inventory_item_data.item.count || 0) + '</td></tr>';
+
+      if (i % 3 == 0) out += '<div class="row">'
+
+      out += '<div class="col s4 center-align">'
+      out += '<img src="image/items/' + bagItems[i].inventory_item_data.item.item_id + '.png" class="item_img"><br />';
+      out += 'Item: ' + itemsArray[bagItems[i].inventory_item_data.item.item_id] + '<br \>'
+      out += 'Count: ' + (bagItems[i].inventory_item_data.item.count || 0);
+      out += '</div>';
+
+      if ((i + 1) % 3 == 0) out += '</div>'
+
     }
-    out += '</table></div></div>';
+
+    out += '</div></div>';
+
     document.getElementById('subcontent').innerHTML = out;
   }
   if (menu == 3) {
@@ -386,8 +397,8 @@ function buildMenu() {
       pkmnImage = pad_with_zeroes(pokedex[i].inventory_item_data.pokedex_entry.pokedex_entry_number, 3) + '.png'
       pkmnName = pokemonArray[pkmnNum-1].Name
       out += '<tr><td><img src="image/pokemon/' + pkmnImage + '" class="png_img"></td><td class="left-align">Name: ' + pkmnName +
-      '<br>Number: ' + pkmnNum + '<br>Times Encountered: ' + pokedex[i].inventory_item_data.pokedex_entry.times_encountered + 
-      '<br>Times Caught: ' + pokedex[i].inventory_item_data.pokedex_entry.times_captured + '</td></tr>';
+          '<br>Number: ' + pkmnNum + '<br>Times Encountered: ' + pokedex[i].inventory_item_data.pokedex_entry.times_encountered +
+          '<br>Times Caught: ' + pokedex[i].inventory_item_data.pokedex_entry.times_captured + '</td></tr>';
     }
     out += '</table></div></div>';
     document.getElementById('subcontent').innerHTML = out;
