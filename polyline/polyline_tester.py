9d565bb283acc8770d57f26aa1632c6d2f45e6bb
Ovidiu Ghinet <ovidiu@ghinet.ch>
2016-07-24 15:36:48 +0200

    modified test

--- polyline/polyline_tester.py
+++ polyline/polyline_tester.py
@@ -5,6 +5,7 @@ from math import ceil
 from polyline_walker import PolylineWalker
 URL = 'https://maps.googleapis.com/maps/api/directions/json?origin=Poststrasse+1,Zug,CH&destination=Poststrasse+9,Zug,CH&mode=walking'
 a = PolylineWalker([x['polyline']['points'] for x in  requests.get(URL).json()['routes'][0]['legs'][0]['steps']], 80)
+print('Walking polyline: ', a.polyline)
 print('Initialted with speed: ', a.speed, 'm/s')
 print('Walking time: ', ceil(sum([haversine.haversine(*x)*1000/a.speed for x in a.walk_steps()])), ' sec.')
 while a.points[-1] != a.get_pos()[0]:
