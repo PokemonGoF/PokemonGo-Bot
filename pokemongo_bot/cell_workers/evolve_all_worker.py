ec56630898bbfea9f42d4f29d482fb07f0d51b11
Guy Zyskind <guy@zyskind.com>
2016-07-24 09:17:18 -0400

    sorting pokemons by cp

--- pokemongo_bot/cell_workers/evolve_all_worker.py
+++ pokemongo_bot/cell_workers/evolve_all_worker.py
@@ -20,6 +20,7 @@ class EvolveAllWorker(object):
         except KeyError:
             pass
         else:
+            self._sort_by_cp(response_dict['responses']['GET_INVENTORY']['inventory_delta']['inventory_items'])
             for item in response_dict['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']:
                 try:
                     reduce(dict.__getitem__, [
@@ -29,10 +30,45 @@ class EvolveAllWorker(object):
                 else:
                     try:
                         pokemon = item['inventory_item_data']['pokemon_data']
-                        self._execute_pokemon_evolve(pokemon, cache)
-                        sleep(1.2)
+                        # TODO: reenable
+                        # self._execute_pokemon_evolve(pokemon, cache)
+                        # sleep(1.2)
                     except:
                         pass
+
+    def _sort_by_cp(self, inventory_items):
+        pokemons = []
+        for item in inventory_items:
+            try:
+                reduce(dict.__getitem__, [
+                       "inventory_item_data", "pokemon_data"], item)
+            except KeyError:
+                pass
+            else:
+                try:
+                    pokemon = item['inventory_item_data']['pokemon_data']
+                    pokemon_num = int(pokemon['pokemon_id']) - 1
+                    pokemon_name = self.bot.pokemon_list[int(pokemon_num)]['Name']
+                    pokemons.append([
+                        pokemon['id'],
+                        pokemon_name,
+                        pokemon['cp']
+                        ])
+                except:
+                    pass
+
+        pokemons.sort(key=lambda x: x[2], reverse=True)
+        ## TODO: remove temp
+        print
+        print
+        for p in pokemons:
+            print p
+        print
+        print
+        ##
+        return pokemons
+
+
 
     def _execute_pokemon_evolve(self, pokemon, cache):
         pokemon_num = int(pokemon['pokemon_id']) - 1
