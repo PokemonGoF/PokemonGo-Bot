d9e2f9c566b4d8c311294b098797ecf2e717350f
earthchie <thanarat@bunditcenter.com>
2016-07-24 15:52:29 +0700

    add [] to date in color print

--- pokemongo_bot/logger.py
+++ pokemongo_bot/logger.py
@@ -16,7 +16,7 @@ def log(string, color = 'white'):
     if color not in colorHex:
         print('[' + time.strftime("%Y-%m-%d %H:%M:%S") + '] '+ string)
     else:
-        print(u'\033['+ colorHex[color] + time.strftime("%Y-%m-%d %H:%M:%S") + ' ' + string.decode('utf-8') + '\033[0m')
+        print(u'\033['+ colorHex[color] + '[' + time.strftime("%Y-%m-%d %H:%M:%S") + '] ' + string.decode('utf-8') + '\033[0m')
     if lcd:
         if(string):
             lcd.message(string)
