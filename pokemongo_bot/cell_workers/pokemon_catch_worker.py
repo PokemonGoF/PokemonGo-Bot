0221f0eac687b35cf315a52acc8cb6c5eba9c618
lightingghost <zhenp3ngzhou@gmail.com>
2016-07-25 01:01:30 -0700

    random catch parameters
    Make catching pokemons not always perfect.

--- pokemongo_bot/cell_workers/pokemon_catch_worker.py
+++ pokemongo_bot/cell_workers/pokemon_catch_worker.py
@@ -5,6 +5,7 @@ from sets import Set
 from utils import distance
 from pokemongo_bot.human_behaviour import sleep
 from pokemongo_bot import logger
+from random import uniform
 
 class PokemonCatchWorker(object):
     BAG_FULL = 'bag_full'
@@ -111,10 +112,10 @@ class PokemonCatchWorker(object):
                             id_list1 = self.count_pokemon_inventory()
                             self.api.catch_pokemon(encounter_id=encounter_id,
                                                    pokeball=pokeball,
-                                                   normalized_reticle_size=1.950,
+                                                   normalized_reticle_size=uniform(1, 3),
                                                    spawn_point_guid=spawnpoint_id,
                                                    hit_pokemon=1,
-                                                   spin_modifier=1,
+                                                   spin_modifier=(0.8, 1),
                                                    NormalizedHitPosition=1)
                             response_dict = self.api.call()
 
