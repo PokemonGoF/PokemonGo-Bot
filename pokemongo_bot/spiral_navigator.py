755bb62158697c65b7cc22ec1f4b16bb42d660ac
Tobias Stumm <stumm@iww.rwth-aachen.de>
2016-07-25 14:32:58 +0200

    Fixes SpiralNavigation skipping waypoints

--- pokemongo_bot/spiral_navigator.py
+++ pokemongo_bot/spiral_navigator.py
@@ -78,6 +78,12 @@ class SpiralNavigator(object):
         if self.x == self.y or self.x < 0 and self.x == -self.y or self.x > 0 and self.x == 1 - self.y:
             (self.dx, self.dy) = (-self.dy, self.dx)
 
-        (self.x, self.y) = (self.x + self.dx, self.y + self.dy)
-        sleep(10)
+        if distance(
+                    i2f(self.api._position_lat),
+                    i2f(self.api._position_lng),
+                    position[0],
+                    position[1]
+                ) < 1:
+            (self.x, self.y) = (self.x + self.dx, self.y + self.dy)
+        sleep(2)
         return position[0:2]
