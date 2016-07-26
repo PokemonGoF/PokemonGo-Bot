1ac292608bc7632ae5972dce08a164ed6608a415
Kelvin <KLVN@users.noreply.github.com>
2016-07-27 00:32:11 +0200

    Using "=" as step indicator (#1032)
    #970 https://github.com/PokemonGoF/PokemonGo-Bot/pull/970#issuecomment-235353129
    
    * updated URL
    
    https://github.com/PokemonGoF/PokemonGo-Bot/pull/1032#discussion-diff-72349483

--- pokemongo_bot/step_walker.py
+++ pokemongo_bot/step_walker.py
@@ -10,8 +10,8 @@ def progress_bar(percentage):
     if not sys.stdout.isatty():
         return
     sys.stdout.write('\r')
-    # http://www.fileformat.info/info/unicode/char/2588/index.htm
-    msg = (u"[%-40s] %d%%" % (u"\u2588"*int(percentage*2//5), percentage))
+    # http://www.fileformat.info/info/unicode/char/003D/index.htm
+    msg = (u"[%-40s] %d%%" % (u"\u003D"*int(percentage*2//5), percentage))
     sys.stdout.write(msg)
     sys.stdout.flush()
 
