fe6f29d9b23072417aac5156a4bf3f3a43a9887a
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-07-26 20:49:09 +0200

    Take into account also the iv to release a pokemon in the initial_transfer

--- pokemongo_bot/cell_workers/initial_transfer_worker.py
+++ pokemongo_bot/cell_workers/initial_transfer_worker.py
@@ -28,11 +28,13 @@ class InitialTransferWorker(object):
 
                 for x in range(1, len(group_cp)):
                     pokemon_name = self.pokemon_list[id - 1]['Name']
-                    if self.should_release_pokemon(pokemon_name, group_cp[x]):
+                    pokemon_data = pokemon_groups[id][group_cp[x]]
+                    pokemon_potential = self.get_pokemon_potential(pokemon_data)
+                    if self.should_release_pokemon(pokemon_name, group_cp[x], pokemon_potential):
                         logger.log('Exchanging {} with {} CP'.format(
                             pokemon_name, group_cp[x]))
                         self.api.release_pokemon(
-                            pokemon_id=pokemon_groups[id][group_cp[x]])
+                            pokemon_id=pokemon_data['id'])
                         response_dict = self.api.call()
                         sleep(2)
 
@@ -57,21 +59,36 @@ class InitialTransferWorker(object):
             except KeyError:
                 continue
 
-            group_id = pokemon['inventory_item_data'][
-                'pokemon_data']['pokemon_id']
-            group_pokemon = pokemon['inventory_item_data'][
-                'pokemon_data']['id']
-            group_pokemon_cp = pokemon[
-                'inventory_item_data']['pokemon_data']['cp']
+            pokemon_data = pokemon['inventory_item_data']['pokemon_data']
+            group_id = pokemon_data['pokemon_id']
+            group_pokemon_cp = pokemon_data['cp']
 
             if group_id not in pokemon_groups:
                 pokemon_groups[group_id] = {}
 
-            pokemon_groups[group_id].update({group_pokemon_cp: group_pokemon})
+            pokemon_groups[group_id].update({group_pokemon_cp: pokemon_data})
         return pokemon_groups
 
-    def should_release_pokemon(self, pokemon_name, cp):
+    def get_pokemon_potential(self, pokemon_data):
+        total_iv = 0
+        iv_stats = ['individual_attack', 'individual_defense', 'individual_stamina']
+        for individual_stat in iv_stats:
+            try:
+                total_iv += pokemon_data[individual_stat]
+            except:
+                continue
+        return round((total_iv / 45.0), 2)
+
+    def should_release_pokemon(self, pokemon_name, cp, iv):
         release_config = self._get_release_config_for(pokemon_name)
+        cp_iv_logic = release_config.get('logic')
+        if not cp_iv_logic:
+            cp_iv_logic = self._get_release_config_for('any').get('logic', 'and')
+
+        release_results = {
+            'cp': False,
+            'iv': False,
+        }
 
         if release_config.get('never_release', False):
             return False
@@ -80,7 +97,28 @@ class InitialTransferWorker(object):
             return True
 
         release_cp = release_config.get('release_below_cp', 0)
-        return cp < release_cp
+        if cp < release_cp:
+            release_results['cp'] = True
+
+        release_iv = release_config.get('release_below_iv', 0)
+        if iv < release_iv:
+            release_results['iv'] = True
+
+        logic_to_function = {
+            'or': lambda x, y: x or y,
+            'and': lambda x, y: x and y
+        }
+
+        #logger.log(
+        #    "Release config for {}: CP {} {} IV {}".format(
+        #        pokemon_name,
+        #        min_cp,
+        #        cp_iv_logic,
+        #        min_iv
+        #    ), 'yellow'
+        #)
+
+        return logic_to_function[cp_iv_logic](*release_results.values())
 
     def _get_release_config_for(self, pokemon):
         release_config = self.config.release.get(pokemon)
