7f1d8d14bc713e1ec0accde8a6f326392c9399c5
Josh Ho <josh@joshho.com>
2016-07-26 02:41:50 -0400

    Revert "Revert "Fix: Without checking if individual_xyz exists, this can throw a KeyError.""
    This reverts commit d281b356fa852cc14660dbb942c376b03e86ffc8.

--- pokemongo_bot/cell_workers/pokemon_catch_worker.py
+++ pokemongo_bot/cell_workers/pokemon_catch_worker.py
@@ -51,11 +51,6 @@ class PokemonCatchWorker(object):
                             if 'pokemon_data' in pokemon and 'cp' in pokemon['pokemon_data']:
                                 cp = pokemon['pokemon_data']['cp']
                                 iv_stats = ['individual_attack', 'individual_defense', 'individual_stamina']
-                                iv_display = '{}/{}/{}'.format(
-                                    pokemon['pokemon_data']['individual_stamina'],
-                                    pokemon['pokemon_data']['individual_attack'],
-                                    pokemon['pokemon_data']['individual_defense']
-                                )
 
                                 for individual_stat in iv_stats:
                                     try:
@@ -72,7 +67,10 @@ class PokemonCatchWorker(object):
                                 logger.log('A Wild {} appeared! [CP {}] [Potential {}]'.format(
                                     pokemon_name, cp, pokemon_potential), 'yellow')
 
-                                logger.log('IV [Stamina/Attack/Defense] = [{}]'.format(iv_display))
+                                logger.log('IV [Stamina/Attack/Defense] = [{}]'.format(
+                                pokemon['pokemon_data']['individual_stamina'],
+                                pokemon['pokemon_data']['individual_attack'],
+                                pokemon['pokemon_data']['individual_defense']))
                                 pokemon['pokemon_data']['name'] = pokemon_name
                                 # Simulate app
                                 sleep(3)
