9d7e02548244e776aed83ef07bc172d7a78ed358
surfaace <hellman.f@gmail.com>
2016-07-26 14:30:17 +0200

    disabling string parsing as structure is about to be changed

--- pokecli.py
+++ pokecli.py
@@ -205,8 +205,8 @@ def init_config():
         return None
 
     # When config.item_filter looks like "101,102,103" needs to be converted to ["101","102","103"]
-    if isinstance(config.item_filter, basestring):
-        config.item_filter= config.item_filter.split(",")
+    #if isinstance(config.item_filter, basestring):
+    #    config.item_filter= config.item_filter.split(",")
 
     # create web dir if not exists
     try:
