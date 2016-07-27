9d9448752518c544c61923a4f282efc4d362526c
anonymouslulz22 <anonlulz22@gmail.com>
2016-07-27 18:03:06 +0530

    as told by douglascamata

--- pokemongo_bot/cell_workers/move_to_fort_worker.py
+++ pokemongo_bot/cell_workers/move_to_fort_worker.py
@@ -40,15 +40,13 @@ class MoveToFortWorker(object):
                     position[1]
                 )
 
-
-            step_walker = self._step_walker
-                if step_walker == True:
-                self.api.player_update(latitude=lat, longitude=lng)
+step_walker = self._step_walker
+            if step_walker == True:
                 response_dict = self.api.call()
-                logger.log('[#] Arrived at Pokestop')
+                logger.log('Arrived at Pokestop')
+	            self.api.player_update(latitude=lat, longitude=lng)
                 return WorkerResult.SUCCESS
-                sleep(2)
-                elif step_walker == False:
+            elif step_walker == False:
                 return WorkerResult.RUNNING
                  
 
