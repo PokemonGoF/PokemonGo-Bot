e44a13afdf9a7e811966f310b1771d8f3aea91aa
surfaace <hellman.f@gmail.com>
2016-07-28 01:31:19 +0200

    seperate worker for transfering pokemons to professor (#1281)
    pulled out initial transfer and transfer on catched pokemon to seperate
    worker

--- pokemongo_bot/cell_workers/pokemon_transfer_worker.py
+++ pokemongo_bot/cell_workers/pokemon_transfer_worker.py
@@ -3,11 +3,12 @@ import json
 from pokemongo_bot.human_behaviour import sleep
 from pokemongo_bot import logger
 
-class InitialTransferWorker(object):
+class PokemonTransferWorker(object):
     def __init__(self, bot):
         self.config = bot.config
         self.pokemon_list = bot.pokemon_list
         self.api = bot.api
+        self.metrics = bot.metrics
 
     def work(self):
         if not self.config.initial_transfer:
@@ -29,17 +30,22 @@ class InitialTransferWorker(object):
                     pokemon_data = pokemon_groups[id][pokemon_cp]
                     pokemon_potential = self.get_pokemon_potential(pokemon_data)
                     if self.should_release_pokemon(pokemon_name, pokemon_cp, pokemon_potential):
-                        message = 'Exchanging {} [CP {}] [Potential {}]'.format(
-                            pokemon_name,
-                            pokemon_cp,
-                            pokemon_potential
-                        )
-                        logger.log(message, 'red')
-                        self.api.release_pokemon(
-                            pokemon_id=pokemon_data['id'])
-                        response_dict = self.api.call()
+                        logger.log('Exchanging {} [CP {}] [Potential {}] for candy!'.format(
+                            pokemon_name, pokemon_cp, pokemon_potential))
+                        self.transfer_pokemon(pokemon_data['id'])
                         sleep(2)
 
+    def release_catched_pokemon(self, pokemon_name, pokemon_to_transfer, cp, iv):
+        # Transfering Pokemon
+        self.transfer_pokemon(pokemon_to_transfer)
+        self.metrics.released_pokemon()
+        logger.log(
+            '{} has been exchanged for candy!'.format(pokemon_name), 'green')
+        
+    def transfer_pokemon(self, pid):
+         self.api.release_pokemon(pokemon_id=pid)
+         response_dict = self.api.call()
+         
     def _initial_transfer_get_groups(self):
         pokemon_groups = {}
         self.api.get_player().get_inventory()
@@ -75,7 +81,7 @@ class InitialTransferWorker(object):
         for individual_stat in iv_stats:
             try:
                 total_iv += pokemon_data[individual_stat]
-            except Exception:
+            except:
                 continue
         return round((total_iv / 45.0), 2)
 
