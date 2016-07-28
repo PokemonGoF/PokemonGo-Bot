6ba163dfe569bfae7490a86b8abfb4385e3754ea
BoGnY <luca_bognolo@modi.it>
2016-07-28 19:38:10 +0200

    Added logging function on files
    Added the function to write every bot output on .txt files

--- pokemongo_bot/logger.py
+++ pokemongo_bot/logger.py
@@ -1,4 +1,5 @@
 import time
+import sys
 try:
     import lcd
     lcd = lcd.lcd()
@@ -8,17 +9,25 @@ except Exception:
     lcd = False
 
 def log(string, color = 'white'):
-    colorHex = {
+	# Write bot output also in a log file
+	try:
+		logs_file = open('logs/' + time.strftime("%Y%m%d") + '.txt', 'a')
+		logs_file.write('[' + time.strftime("%H:%M:%S") + '] ' + string + '\n')
+		logs_file.close()
+	except:
+		print "Unknown error while writing logs file!"
+    
+	colorHex = {
         'red': '91m',
         'green': '92m',
         'yellow': '93m',
         'blue': '94m',
         'cyan': '96m'
     }
-    if color not in colorHex:
-        print('[' + time.strftime("%H:%M:%S") + '] '+ string)
-    else:
-        print('[' + time.strftime("%H:%M:%S") + '] ' + u'\033['+ colorHex[color] + string.decode('utf-8') + '\033[0m')
-    if lcd:
-        if(string):
-            lcd.message(string)
+	if color not in colorHex:
+		print('[' + time.strftime("%H:%M:%S") + '] '+ string)
+	else:
+		print('[' + time.strftime("%H:%M:%S") + '] ' + u'\033['+ colorHex[color] + string.decode('utf-8') + '\033[0m')
+	if lcd:
+		if(string):
+			lcd.message(string)
