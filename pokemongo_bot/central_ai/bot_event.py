1c8f50103d513677e426bba37c39d5e456984bde
Simba Zhang <solderzzc@gmail.com>
2016-07-27 17:59:01 -0700

    Fixed typo in warning info.

--- pokemongo_bot/central_ai/bot_event.py
+++ pokemongo_bot/central_ai/bot_event.py
@@ -12,7 +12,7 @@ class BotEvent(object):
         # report to server
         # Review Very Carefully for the following line, forbiden ID changed PR:
         if bot.config.health_record:
-            logger.log('[x] Send anonymous bot health report to server, it can be disabled by setting \"health_record\"=false in config file', 'red')
+            logger.log('[x] Send anonymous bot health report to server, it can be disabled by config \"health_record\":false in config file', 'red')
             logger.log('[x] Wait for 2 seconds ', 'red')
             sleep(3)
             self.tracker = Tracker.create('UA-81469507-1', use_post=True)
