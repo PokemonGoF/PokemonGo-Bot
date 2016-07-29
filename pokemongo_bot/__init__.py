0baebd479f742d8f51c08f1c0a3f901cb59a212e
klingan <klingan@mailinator.com>
2016-07-29 20:52:20 +0200

    Remove extra newline with each tick

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -84,7 +84,7 @@ class PokemonGoBot(object):
         # self.event_manager.emit('location', 'level'='info', data={'lat': 1, 'lng':1}),
 
     def tick(self):
-        logger.log('')
+        #logger.log('')
         self.cell = self.get_meta_cell()
         self.tick_count +=1
 
