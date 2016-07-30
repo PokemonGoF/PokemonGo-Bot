7b3f70f4e8e9a58d90d8367e4a75348503e98597
Thanh TRAN <thanhtung.tran100@gmail.com>
2016-07-30 22:53:58 +0200

    Possibility to set another config with run script (#1899)

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-# Assumes the config file is created and named config.json
 # Starts PokemonGo-Bot
-eval "python pokecli.py --config ./configs/config.json"
+config=""
+
+if [ ! -z $1 ]; then
+    config=$1
+else
+    config="./configs/config.json"
+    if [ ! -f ${config} ]; then
+        echo -e "There's no ./configs/config.json file"
+        echo -e "Please create one or use another config file"
+        echo -e "./run.sh [path/to/config/file]"
+        exit 1
+    fi
+fi
+
+python pokecli.py --config ${config}
