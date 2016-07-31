760787e858050d0a9382cc6f0c6a0a1610e198e1
Nikhil Pandey <nikhilpandey2009@gmail.com>
2016-07-31 19:35:06 +0545

    Snipe Pokemons from List

--- pokemongo_bot/cell_workers/snipe_pokemon.py
+++ pokemongo_bot/cell_workers/snipe_pokemon.py
@@ -17,22 +17,53 @@ class SnipePokemon(BaseTask):
         self.position = self.bot.position
 
     def work(self):
-        if self.bot.tick_count is not 1:
+        if self.bot.config.snipe_list == None:
+            return
+        self.start_sniping()
+
+    def start_sniping(self):
+        try:
+            logger.log('Reading snipping list.', 'green')
+            with open(self.bot.config.snipe_list, 'r+') as f:
+                try:
+                    locations_json = json.load(f)
+                except ValueError:
+                    logger.log('Invalid json file', 'error')
+                    return
+
+                try:
+                    locations = locations_json['locations']
+                except KeyError:
+                    logger.log('Failed to parse sniping locations', 'red')
+                    return
+
+                if isinstance(locations, list) and len(locations):
+                    while locations:
+                        location = locations.pop(0)
+                        logger.log('Found location: ' + location, 'green')
+                        location = location.replace(' ', '')
+                        pattern = '^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$'
+                        if not re.match(pattern, location):
+                            logger.log('Wrong format location!', 'red')
+                            continue
+                        self.snipe_pokemon(location)
+                        f.seek(0)
+                        try:
+                            json.dump(locations_json, f)
+                        except IOError:
+                            logger.log('Failed to remove location from snipe list.', 'red')
+                            return
+                        except:
+                            logger.log('Unknown Error occurred attempting to remove location from snipe list.', 'red')
+                        f.truncate()
+                else:
+                    logger.log('No locations to snipe!', 'yellow')
+                return
+        except IOError:
+            logger.log('Error reading sniping list!', 'red')
             return
 
-        while True:
-            location = raw_input('Enter snipe location: ')
-            location = location.replace(' ', '')
-
-            pattern = "^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$"
-            if re.match(pattern, location):
-                self.snipe_pokemon(location)
-            else:
-                logger.log('Wrong format location!', 'red')
-                break
-
-
-    def snipe_pokemon(self, location, delay=2): 
+    def snipe_pokemon(self, location, delay=2):
         prevPosition = self.bot.position
 
         # Check if session token has expired
@@ -67,7 +98,7 @@ class SnipePokemon(BaseTask):
 
             # Try to catch VIP pokemon
             for pokemon in self.cell['catchable_pokemons']:
-                pokemon_num = int(pokemon['pokemon_id']) - 1   
+                pokemon_num = int(pokemon['pokemon_id']) - 1
                 pokemon_name = self.pokemon_list[int(pokemon_num)]['Name']
                 vip_name = self.bot.config.vips.get(pokemon_name)
                 if vip_name == {}:
@@ -77,7 +108,7 @@ class SnipePokemon(BaseTask):
                     # if VIP pokemon is nearest, break loop
                     if pokemon == self.cell['catchable_pokemons'][0]:
                         break
-        
+
         if 'wild_pokemons' in self.cell and len(self.cell['wild_pokemons']) > 0 and not catch_pokemon:
             # Sort all by distance from current pos- eventually this should
             # build graph & A* it
@@ -89,7 +120,7 @@ class SnipePokemon(BaseTask):
         if not catch_pokemon:
             logger.log('No pokemon found!', 'yellow')
             time.sleep(delay)
-            # go back 
+            # go back
             self.api.set_position(*prevPosition)
             time.sleep(delay)
             self.bot.heartbeat()
@@ -97,12 +128,12 @@ class SnipePokemon(BaseTask):
             return None
 
         catchWorker = PokemonCatchWorker(catch_pokemon, self.bot)
-        
+
         logger.log('Encounter pokemon', 'green')
         apiEncounterResponse = catchWorker.create_encounter_api_call()
 
         time.sleep(delay)
-        # go back 
+        # go back
         logger.log('Teleport back previous location..', 'green')
         self.api.set_position(*prevPosition)
         # wait for go back
