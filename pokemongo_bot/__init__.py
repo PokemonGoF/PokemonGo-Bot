1c4826ce64a7218b2d309070ba32035789638433
Eli White <github@eli-white.com>
2016-07-30 18:05:18 -0700

    Configure the tasks from config.json

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -58,8 +58,6 @@ class PokemonGoBot(object):
         elif self.config.navigator_type == 'path':
             self.navigator = cell_workers.FollowPath
 
-        self._setup_workers()
-
         random.seed()
 
     def _setup_event_system(self):
@@ -295,21 +293,6 @@ class PokemonGoBot(object):
         self.update_inventory()
         # send empty map_cells and then our position
         self.update_web_location()
-
-    def _setup_workers(self):
-        self.workers = [
-            cell_workers.HandleSoftBan(self),
-            cell_workers.IncubateEggs(self),
-            cell_workers.TransferPokemon(self),
-            cell_workers.EvolveAll(self),
-            cell_workers.RecycleItems(self),
-            cell_workers.CatchVisiblePokemon(self),
-            cell_workers.SpinFort(self),
-            cell_workers.MoveToFort(self),
-            cell_workers.CatchLuredPokemon(self),
-            cell_workers.SpinFort(self),
-            self.navigator(self)
-        ]
 
     def _print_character_info(self):
         # get player profile call
