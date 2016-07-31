a3eab83db76c6af62bf2e0e5c320c76d29c7bf81
z4ppy <z4ppy.bbc@gmail.com>
2016-07-31 16:11:31 +0200

    Add simple lure attraction feature

--- pokemongo_bot/cell_workers/move_to_fort.py
+++ pokemongo_bot/cell_workers/move_to_fort.py
@@ -7,6 +7,12 @@ from utils import distance, format_dist, fort_details
 
 
 class MoveToFort(BaseTask):
+    lure_distance = 0
+    def initialize(self):
+        self.lure_distance = 0
+        self.lure_attraction = self.config.get("lure_attraction", True)
+        self.lure_max_distance = self.config.get("lure_max_distance", 2000)
+
     def should_run(self):
         return (self.bot.has_space_for_loot()) or self.bot.softban
 
@@ -35,7 +41,12 @@ class MoveToFort(BaseTask):
         )
 
         if dist > Constants.MAX_DISTANCE_FORT_IS_REACHABLE:
-            logger.log('Moving towards fort {}, {} left'.format(fort_name, format_dist(dist, unit)))
+            if self.lure_distance > 0:
+                lured_str = ' (under the attraction of lure: {})'.format(format_dist(self.lure_distance, unit))
+            else:
+                lured_str = ''
+
+            logger.log('Moving towards fort {}, {} left{}'.format(fort_name, format_dist(dist, unit), lured_str))
 
             step_walker = StepWalker(
                 self.bot,
@@ -55,10 +66,45 @@ class MoveToFort(BaseTask):
 
         # Remove stops that are still on timeout
         forts = filter(lambda x: x["id"] not in self.bot.fort_timeouts, forts)
+        lures = filter(lambda x: True if x.get('lure_info', None) != None else False, forts)
 
         # Remove all forts which were spun in the last ticks to avoid circles if set
         if self.bot.config.forts_avoid_circles:
             forts = filter(lambda x: x["id"] not in self.bot.recent_forts, forts)
+
+        if (len(lures) and self.lure_attraction):
+            dist_lure_me = distance(self.bot.position[0], self.bot.position[1],
+                                    lures[0]['latitude'],lures[0]['longitude'])
+        else:
+            dist_lure_me = 0
+
+        # add lure attraction if needed
+        if dist_lure_me > 0 and dist_lure_me < self.lure_max_distance:
+
+            self.lure_distance = dist_lure_me
+
+            for fort in forts:
+                dist_lure_fort = distance(
+                    fort['latitude'],
+                    fort['longitude'],
+                    lures[0]['latitude'],
+                    lures[0]['longitude'])
+                dist_fort_me = distance(
+                    fort['latitude'],
+                    fort['longitude'],
+                    self.bot.position[0],
+                    self.bot.position[1])
+
+                if dist_lure_fort < dist_lure_me and dist_lure_me > dist_fort_me:
+                    return fort
+
+                if dist_fort_me > dist_lure_me:
+                    break
+
+            return lures[0]
+
+        else:
+            self.lure_distance = 0
 
         if len(forts) > 0:
             return forts[0]
