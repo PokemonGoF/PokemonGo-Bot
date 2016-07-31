dc804e681c4485675d63be00c641db349f198a99
mfizz <mitchfizz@gmail.com>
2016-07-31 16:28:14 +0200

    trying to get travis to build

--- pokemongo_bot/test/follow_cluster_test.py
+++ pokemongo_bot/test/follow_cluster_test.py
@@ -7,7 +7,7 @@ class FollowClusterTestCase(unittest.TestCase):
 
     @patch('pokemongo_bot.PokemonGoBot')
     def testWorkAway(self, mock_pokemongo_bot):
-        forts_path = os.path.join('resources', 'example_forts.pickle')
+        forts_path = os.path.join(os.path.curdir, 'resources', 'example_forts.pickle')
         with open(forts_path, 'rb') as forts:
             ex_forts = pickle.load(forts)
         config = {'radius': 50}
