e8eeb752f46c93497480e56456bef50970edb39d
Aurélien Chrétien <aurelien@recatch.tv>
2016-08-01 18:09:40 +0200

    Refactor evolve_all worker
    - Remove transfer of evolved pokemon (should be handle by transfer task)
    - Add order_by config flag to choose to evolve by iv or cp (default: cp)
    - Add evolve_iv_min as threshold for evolve by iv (order by cp under threshold)
    - Fix _validate_config not called before
    - Get candy list to test if enough candy in the bag
    - Filter out pokemon which can't be evolved

--- pokemongo_bot/cell_workers/evolve_all.py
+++ pokemongo_bot/cell_workers/evolve_all.py
@@ -7,7 +7,9 @@ class EvolveAll(BaseTask):
     def initialize(self):
         self.evolve_all = self.config.get('evolve_all', [])
         self.evolve_speed = self.config.get('evolve_speed', 3.7)
+        self.order_by = self.config.get('order_by', 'cp')
         self.evolve_cp_min = self.config.get('evolve_cp_min', 300)
+        self.evolve_iv_min = self.config.get('evolve_iv_min', 0.8)
         self.use_lucky_egg = self.config.get('use_lucky_egg', False)
         self._validate_config()
 
@@ -20,61 +22,49 @@ class EvolveAll(BaseTask):
             return
 
         response_dict = self.bot.get_inventory()
+        inventory_items = response_dict.get('responses', {}).get('GET_INVENTORY', {}).get('inventory_delta', {}).get(
+            'inventory_items', {})
+
+        evolve_list = self._sort_and_filter(inventory_items)
+
+        if self.evolve_all[0] != 'all':
+            # filter out non-listed pokemons
+            evolve_list = filter(lambda x: x["name"] in self.evolve_all, evolve_list)
+
+        # enable to limit number of pokemons to evolve. Useful for testing.
+        nn = 2
+        if len(evolve_list) > nn:
+            evolve_list = evolve_list[:nn]
+
         cache = {}
+        candy_list = self._get_candy_list(inventory_items)
+        id_list1 = self._pokemon_ids_list()
+        for pokemon in evolve_list:
+            if self._can_evolve(pokemon, candy_list, cache):
+                self._execute_pokemon_evolve(pokemon, candy_list, cache)
 
-        try:
-            reduce(dict.__getitem__, [
-                "responses", "GET_INVENTORY", "inventory_delta", "inventory_items"], response_dict)
-        except KeyError:
-            pass
-        else:
-            evolve_list = self._sort_by_cp_iv(
-                response_dict['responses']['GET_INVENTORY']['inventory_delta']['inventory_items'])
-            if self.evolve_all[0] != 'all':
-                # filter out non-listed pokemons
-                evolve_list = [x for x in evolve_list if str(x[1]) in self.evolve_all]
+        id_list2 = self._pokemon_ids_list()
+        release_cand_list_ids = list(set(id_list2) - set(id_list1))
 
-            # enable to limit number of pokemons to evolve. Useful for testing.
-            # nn = 3
-            # if len(evolve_list) > nn:
-            #     evolve_list = evolve_list[:nn]
-            #
-
-            id_list1 = self.count_pokemon_inventory()
-            for pokemon in evolve_list:
-                try:
-                    self._execute_pokemon_evolve(pokemon, cache)
-                except Exception:
-                    pass
-            id_list2 = self.count_pokemon_inventory()
-            release_cand_list_ids = list(set(id_list2) - set(id_list1))
-
-            if release_cand_list_ids:
-                logger.log('[#] Evolved {} pokemons! Checking if any of them needs to be released ...'.format(
-                    len(release_cand_list_ids)
-                ))
-                self._release_evolved(release_cand_list_ids)
+        if release_cand_list_ids:
+            logger.log('Evolved {} pokemon!'.format(len(release_cand_list_ids)))
 
     def _should_run(self):
-        # Will skip evolving if user wants to use an egg and there is none
-        if not self.evolve_all:
+        if not self.evolve_all or self.evolve_all[0] == 'none':
             return False
 
-        # Evolve all is used - Don't run after the first tick or if the config flag is false
+        # Evolve all is used - Use Lucky egg only at the first tick
         if self.bot.tick_count is not 1 or not self.use_lucky_egg:
             return True
 
         lucky_egg_count = self.bot.item_inventory_count(Item.ITEM_LUCKY_EGG.value)
 
-        # Lucky Egg should only be popped at the first tick
         # Make sure the user has a lucky egg and skip if not
         if lucky_egg_count > 0:
             logger.log('Using lucky egg ... you have {}'.format(lucky_egg_count))
             response_dict_lucky_egg = self.bot.use_lucky_egg()
-            if response_dict_lucky_egg and 'responses' in response_dict_lucky_egg and \
-                            'USE_ITEM_XP_BOOST' in response_dict_lucky_egg['responses'] and \
-                            'result' in response_dict_lucky_egg['responses']['USE_ITEM_XP_BOOST']:
-                result = response_dict_lucky_egg['responses']['USE_ITEM_XP_BOOST']['result']
+            if response_dict_lucky_egg:
+                result = response_dict_lucky_egg.get('responses', {}).get('USE_ITEM_XP_BOOST', {}).get('result', 0)
                 if result is 1:  # Request success
                     logger.log('Successfully used lucky egg... ({} left!)'.format(lucky_egg_count - 1), 'green')
                     return True
@@ -86,207 +76,98 @@ class EvolveAll(BaseTask):
             logger.log('No lucky eggs... skipping evolve!', 'yellow')
             return False
 
-    def _release_evolved(self, release_cand_list_ids):
-        response_dict = self.bot.get_inventory()
-        cache = {}
-
-        try:
-            reduce(dict.__getitem__, [
-                "responses", "GET_INVENTORY", "inventory_delta", "inventory_items"], response_dict)
-        except KeyError:
-            pass
-        else:
-            release_cand_list = self._sort_by_cp_iv(
-                response_dict['responses']['GET_INVENTORY']['inventory_delta']['inventory_items'])
-            release_cand_list = [x for x in release_cand_list if x[0] in release_cand_list_ids]
-
-            ## at this point release_cand_list contains evolved pokemons data
-            for cand in release_cand_list:
-                pokemon_id = cand[0]
-                pokemon_name = cand[1]
-                pokemon_cp = cand[2]
-                pokemon_potential = cand[3]
-
-                if self.should_release_pokemon(pokemon_name, pokemon_cp, pokemon_potential):
-                    # Transfering Pokemon
-                    self.transfer_pokemon(pokemon_id)
-                    logger.log(
-                        '[#] {} has been exchanged for candy!'.format(pokemon_name), 'red')
-
-    def _sort_by_cp_iv(self, inventory_items):
-        pokemons1 = []
-        pokemons2 = []
+    def _get_candy_list(self, inventory_items):
+        candies = {}
         for item in inventory_items:
-            try:
-                reduce(dict.__getitem__, [
-                    "inventory_item_data", "pokemon_data"], item)
-            except KeyError:
-                pass
-            else:
-                try:
-                    pokemon = item['inventory_item_data']['pokemon_data']
-                    pokemon_num = int(pokemon['pokemon_id']) - 1
-                    pokemon_name = self.bot.pokemon_list[int(pokemon_num)]['Name']
-                    v = [
-                        pokemon['id'],
-                        pokemon_name,
-                        pokemon['cp'],
-                        self._compute_iv(pokemon)
-                    ]
-                    if pokemon['cp'] > self.evolve_cp_min:
-                        pokemons1.append(v)
-                    else:
-                        pokemons2.append(v)
-                except Exception:
-                    pass
+            candy = item.get('inventory_item_data', {}).get('candy', {})
+            family_id = candy.get('family_id', 0)
+            amount = candy.get('candy', 0)
+            if family_id > 0 and amount > 0:
+                family = self.bot.pokemon_list[family_id - 1]['Name'] + " candies"
+                candies[family] = amount
 
-        # Sort larger CP pokemons by IV, tie breaking by CP
-        pokemons1.sort(key=lambda x: (x[3], x[2]), reverse=True)
+        return candies
 
-        # Sort smaller CP pokemons by CP, tie breaking by IV
-        pokemons2.sort(key=lambda x: (x[2], x[3]), reverse=True)
+    def _sort_and_filter(self, inventory_items):
+        pokemons = []
+        for item in inventory_items:
+            pokemon = item.get('inventory_item_data', {}).get('pokemon_data', {})
+            pokemon_num = int(pokemon.get('pokemon_id', 0)) - 1
+            next_evol = self.bot.pokemon_list[pokemon_num].get('Next Evolution Requirements', {})
+            pokemons.append({
+                'id': pokemon.get('id', 0),
+                'num': pokemon_num,
+                'name': self.bot.pokemon_list[pokemon_num]['Name'],
+                'cp': pokemon.get('cp', 0),
+                'iv': self._compute_iv(pokemon),
+                'candies_family': next_evol.get('Name', ""),
+                'candies_amount': next_evol.get('Amount', 0)
+            })
 
-        return pokemons1 + pokemons2
+        pokemons = filter(lambda x: x["id"] > 0 and x["cp"] > 0 and x["iv"] > 0 and x["candies_amount"] > 0, pokemons)
 
-    def _execute_pokemon_evolve(self, pokemon, cache):
-        pokemon_id = pokemon[0]
-        pokemon_name = pokemon[1]
-        pokemon_cp = pokemon[2]
-        pokemon_iv = pokemon[3]
+        if self.order_by == "cp":
+            sort_param_a = "cp"
+            sort_param_b = "iv"
+            threshold = self.evolve_cp_min
+        else:
+            sort_param_a = "iv"
+            sort_param_b = "cp"
+            threshold = self.evolve_iv_min
+
+        pokemons.sort(key=lambda x: (x['num'], 1, x[sort_param_a], x[sort_param_b]) if x[sort_param_a] > threshold
+        else (x['num'], 0, x[sort_param_b], x[sort_param_a]), reverse=True)
+
+        return pokemons
+
+    def _can_evolve(self, pokemon, candy_list, cache):
+
+        if pokemon["name"] in cache:
+            return False
+
+        family = pokemon["candies_family"]
+        amount = pokemon["candies_amount"]
+        if family in candy_list and candy_list[family] >= amount:
+            return True
+        else:
+            cache[pokemon["name"]] = 1
+            return False
+
+    def _execute_pokemon_evolve(self, pokemon, candy_list, cache):
+        pokemon_id = pokemon["id"]
+        pokemon_name = pokemon["name"]
+        pokemon_cp = pokemon["cp"]
+        pokemon_iv = pokemon["iv"]
 
         if pokemon_name in cache:
             return
 
         self.bot.api.evolve_pokemon(pokemon_id=pokemon_id)
         response_dict = self.bot.api.call()
-        status = response_dict['responses']['EVOLVE_POKEMON']['result']
-        if status == 1:
-            logger.log('[#] Successfully evolved {} with {} CP and {} IV!'.format(
-                pokemon_name, pokemon_cp, pokemon_iv
-            ))
-
+        if response_dict.get('responses', {}).get('EVOLVE_POKEMON', {}).get('result', 0) == 1:
+            logger.log('Successfully evolved {} with {} CP and {} IV!'.format(pokemon_name, pokemon_cp, pokemon_iv))
+            candy_list[pokemon["candies_family"]] -= pokemon["candies_amount"]
             sleep(self.evolve_speed)
-
         else:
             # cache pokemons we can't evolve. Less server calls
             cache[pokemon_name] = 1
             sleep(0.7)
 
-    # TODO: move to utils. These methods are shared with other workers.
-    def transfer_pokemon(self, pid):
-        self.bot.api.release_pokemon(pokemon_id=pid)
-        response_dict = self.bot.api.call()
-
-    def count_pokemon_inventory(self):
+    def _pokemon_ids_list(self):
         response_dict = self.bot.get_inventory()
         id_list = []
-        return self.counting_pokemon(response_dict, id_list)
-
-    def counting_pokemon(self, response_dict, id_list):
-        try:
-            reduce(dict.__getitem__, [
-                "responses", "GET_INVENTORY", "inventory_delta", "inventory_items"], response_dict)
-        except KeyError:
-            pass
-        else:
-            for item in response_dict['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']:
-                try:
-                    reduce(dict.__getitem__, [
-                        "inventory_item_data", "pokemon_data"], item)
-                except KeyError:
-                    pass
-                else:
-                    pokemon = item['inventory_item_data']['pokemon_data']
-                    if pokemon.get('is_egg', False):
-                        continue
-                    id_list.append(pokemon['id'])
+        inventory_items = response_dict.get('responses', {}).get('GET_INVENTORY', {}).get('inventory_delta', {}).get(
+            'inventory_items', {})
+        for item in inventory_items:
+            pokemon = item.get('inventory_item_data', {}).get('pokemon_data', {})
+            if pokemon.get('is_egg', False):
+                continue
+            id_list.append(pokemon.get('id'))
 
         return id_list
 
-    def should_release_pokemon(self, pokemon_name, cp, iv):
-        if self._check_always_capture_exception_for(pokemon_name):
-            return False
-        else:
-            release_config = self._get_release_config_for(pokemon_name)
-            cp_iv_logic = release_config.get('logic')
-            if not cp_iv_logic:
-                cp_iv_logic = self._get_release_config_for('any').get('logic', 'and')
-
-            release_results = {
-                'cp': False,
-                'iv': False,
-            }
-
-            if 'release_below_cp' in release_config:
-                min_cp = release_config['release_below_cp']
-                if cp < min_cp:
-                    release_results['cp'] = True
-
-            if 'release_below_iv' in release_config:
-                min_iv = release_config['release_below_iv']
-                if iv < min_iv:
-                    release_results['iv'] = True
-
-            if release_config.get('always_release'):
-                return True
-
-            logic_to_function = {
-                'or': lambda x, y: x or y,
-                'and': lambda x, y: x and y
-            }
-
-            # logger.log(
-            #    "[x] Release config for {}: CP {} {} IV {}".format(
-            #        pokemon_name,
-            #        min_cp,
-            #        cp_iv_logic,
-            #        min_iv
-            #    ), 'yellow'
-            # )
-
-            return logic_to_function[cp_iv_logic](*release_results.values())
-
-    def _get_release_config_for(self, pokemon):
-        release_config = self.bot.config.release.get(pokemon)
-        if not release_config:
-            release_config = self.bot.config.release['any']
-        return release_config
-
-    def _get_exceptions(self):
-        exceptions = self.bot.config.release.get('exceptions')
-        if not exceptions:
-            return None
-        return exceptions
-
-    def _get_always_capture_list(self):
-        exceptions = self._get_exceptions()
-        if not exceptions:
-            return []
-        always_capture_list = exceptions['always_capture']
-        if not always_capture_list:
-            return []
-        return always_capture_list
-
-    def _check_always_capture_exception_for(self, pokemon_name):
-        always_capture_list = self._get_always_capture_list()
-        if not always_capture_list:
-            return False
-        else:
-            for pokemon in always_capture_list:
-                if pokemon_name == str(pokemon):
-                    return True
-        return False
-
     # TODO: should also go to util and refactor in catch worker
     def _compute_iv(self, pokemon):
-        total_IV = 0.0
-        iv_stats = ['individual_attack', 'individual_defense', 'individual_stamina']
-
-        for individual_stat in iv_stats:
-            try:
-                total_IV += pokemon[individual_stat]
-            except Exception:
-                pokemon[individual_stat] = 0
-                continue
-        pokemon_potential = round((total_IV / 45.0), 2)
-        return pokemon_potential
+        total_iv = pokemon.get("individual_attack", 0) + pokemon.get("individual_stamina", 0) + pokemon.get(
+            "individual_defense", 0)
+        return round((total_iv / 45.0), 2)
