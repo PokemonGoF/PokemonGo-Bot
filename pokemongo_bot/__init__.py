2ce7d3f25abd0ec22a413681ce4ad7a708b3a791
mfizz <mitchfizz@gmail.com>
2016-08-01 18:27:29 +0200

    adapted improper exception use

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -413,15 +413,11 @@ class PokemonGoBot(object):
         items_stock = {x.value: 0 for x in list(Item)}
 
         for item in inventory_dict:
-            try:
-                # print(item['inventory_item_data']['item'])
-                item_id = item['inventory_item_data']['item']['item_id']
-                item_count = item['inventory_item_data']['item']['count']
-
+            item_id = item.get('inventory_item_data', {}).get('item', {}).get('item_id', {})
+            item_count = item.get('inventory_item_data', {}).get('item', {}).get('count', {})
+            if item_count != {} and item_id != {}:
                 if item_id in items_stock:
                     items_stock[item_id] = item_count
-            except Exception:
-                continue
         return items_stock
 
     def item_inventory_count(self, id):
