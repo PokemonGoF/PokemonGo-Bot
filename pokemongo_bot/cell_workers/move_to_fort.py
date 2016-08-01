62caabf4a9813b54256f3f1f5f6f40ed1a477941
z4ppy <z4ppy.bbc@gmail.com>
2016-08-01 20:27:06 +0200

    Update move_to_fort.py

--- pokemongo_bot/cell_workers/move_to_fort.py
+++ pokemongo_bot/cell_workers/move_to_fort.py
@@ -15,8 +15,8 @@ class MoveToFort(BaseTask):
 
     def should_run(self):
         has_space_for_loot = self.bot.has_space_for_loot()
-        #if not has_space_for_loot:
-            #logger.log("Not moving to any forts as there aren't enough space. You might want to change your config to recycle more items if this message appears consistently.", 'yellow')
+        if not has_space_for_loot:
+            logger.log("Not moving to any forts as there aren't enough space. You might want to change your config to recycle more items if this message appears consistently.", 'yellow')
         return has_space_for_loot or self.bot.softban
 
     def is_attracted(self):
