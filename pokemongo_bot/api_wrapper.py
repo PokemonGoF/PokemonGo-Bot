7f05d4c2e3398f0a81ec571f50328f61d1ac97ec
ben <benj.groeneveld@gmail.com>
2016-08-02 13:40:44 +0200

    wip: fixing imports and tests

--- pokemongo_bot/api_wrapper.py
+++ pokemongo_bot/api_wrapper.py
@@ -1,33 +1,52 @@
 import time
 
-from pgoapi.exceptions import (NotLoggedInException,
-                               ServerBusyOrOfflineException)
+from pgoapi.exceptions import NotLoggedInException, ServerBusyOrOfflineException, NoPlayerPositionSetException, EmptySubrequestChainException
+
+from pgoapi import PGoApi, PGoApiRequest, RpcApi
+from POGOProtos.Networking.Requests_pb2 import RequestType
 
 import logger
 from human_behaviour import sleep
 
+class ApiWrapper(PGoApi):
+    def create_request(self):
+        request = ApiRequest(
+            self._api_endpoint,
+            self._auth_provider,
+            self._position_lat,
+            self._position_lng,
+            self._position_alt
+        )
+        return request
 
-class ApiWrapper(object):
-    def __init__(self, api):
-        self._api = api
+    def reset_auth(self):
+        self._auth_token = None
+        self._auth_provider = None
+        self._api_endpoint = None
+
+
+class ApiRequest(PGoApiRequest):
+    def __init__(self, *args):
+        super(ApiRequest, self).__init__(*args)
         self.request_callers = []
-        self.reset_auth()
         self.last_api_request_time = None
         self.requests_per_seconds = 2
 
-    def reset_auth(self):
-        self._api._auth_token = None
-        self._api._auth_provider = None
-        self._api._api_endpoint = None
+    def can_call(self):
+        if not self._req_method_list:
+            raise EmptySubrequestChainException()
 
-    # wrap api methods
-    def _can_call(self):
-        if not self._api._req_method_list or len(self._api._req_method_list) == 0:
-            raise RuntimeError('Trying to call the api without setting any request')
-        if self._api._auth_provider is None or not self._api._auth_provider.is_login():
-            logger.log('Not logged in!', 'red')
+        if (self._position_lat is None) or (self._position_lng is None) or (self._position_alt is None):
+            raise NoPlayerPositionSetException()
+
+        if self._auth_provider is None or not self._auth_provider.is_login():
+            self.log.info('Not logged in')
             raise NotLoggedInException()
+
         return True
+
+    def _call(self):
+        return super(ApiRequest, self).call()
 
     def _pop_request_callers(self):
         r = self.request_callers
@@ -54,22 +73,22 @@ class ApiWrapper(object):
 
     def call(self, max_retry=5):
         request_callers = self._pop_request_callers()
-        if not self._can_call():
+        if not self.can_call():
             return False # currently this is never ran, exceptions are raised before
 
         request_timestamp = None
-
-        api_req_method_list = self._api._req_method_list
+        api_req_method_list = self._req_method_list
         result = None
         try_cnt = 0
+
         while True:
             request_timestamp = self.throttle_sleep()
-            self._api._req_method_list = [req_method for req_method in api_req_method_list] # api internally clear this field after a call
-            result = self._api.call()
-            if not self._is_response_valid(result, request_callers):
+            # self._call internally clear this field, so save it
+            self._req_method_list = [req_method for req_method in api_req_method_list]
+            result = self._call()
+            if not self.is_response_valid(result, request_callers):
                 try_cnt += 1
                 logger.log('Server seems to be busy or offline - try again - {}/{}'.format(try_cnt, max_retry), 'red')
-
                 if try_cnt >= max_retry:
                     raise ServerBusyOrOfflineException()
                 sleep(1)
@@ -79,15 +98,10 @@ class ApiWrapper(object):
         self.last_api_request_time = request_timestamp
         return result
 
-    def login(self, provider, username, password):
-        return self._api.login(provider, username, password)
-
-    # fallback
     def __getattr__(self, func):
-        DEFAULT_ATTRS = ['_position_lat', '_position_lng', '_auth_provider', '_api_endpoint', 'set_position', 'get_position']
-        if func not in DEFAULT_ATTRS:
+        if func.upper() in  RequestType.keys():
             self.request_callers.append(func)
-        return getattr(self._api, func)
+        return super(ApiRequest, self).__getattr__(func)
 
     def throttle_sleep(self):
         now_milliseconds = time.time() * 1000
@@ -100,3 +114,98 @@ class ApiWrapper(object):
             time.sleep(sleep_time / 1000)
 
         return now_milliseconds
+
+
+# class ApiWrapper(object):
+#     def __init__(self, api):
+#         self._api = api
+#         self.request_callers = []
+#         self.reset_auth()
+#         self.last_api_request_time = None
+#         self.requests_per_seconds = 2
+
+#     def reset_auth(self):
+#         self._api._auth_token = None
+#         self._api._auth_provider = None
+#         self._api._api_endpoint = None
+
+#     # wrap api methods
+#     def _can_call(self):
+#         if not self._api._req_method_list or len(self._api._req_method_list) == 0:
+#             raise RuntimeError('Trying to call the api without setting any request')
+#         if self._api._auth_provider is None or not self._api._auth_provider.is_login():
+#             logger.log('Not logged in!', 'red')
+#             raise NotLoggedInException()
+#         return True
+
+#     def _pop_request_callers(self):
+#         r = self.request_callers
+#         self.request_callers = []
+#         return [i.upper() for i in r]
+
+#     def _is_response_valid(self, result, request_callers):
+#         if not result or result is None or not isinstance(result, dict):
+#             return False
+
+#         if not 'responses' in result or not 'status_code' in result:
+#             return False
+
+#         if not isinstance(result['responses'], dict):
+#             return False
+
+#         # the response can still programatically be valid at this point
+#         # but still be wrong. we need to check if the server did sent what we asked it
+#         for request_caller in request_callers:
+#             if not request_caller in result['responses']:
+#                 return False
+
+#         return True
+
+#     def call(self, max_retry=5):
+#         request_callers = self._pop_request_callers()
+#         if not self._can_call():
+#             return False # currently this is never ran, exceptions are raised before
+
+#         request_timestamp = None
+
+#         api_req_method_list = self._api._req_method_list
+#         result = None
+#         try_cnt = 0
+#         while True:
+#             request_timestamp = self.throttle_sleep()
+#             self._api._req_method_list = [req_method for req_method in api_req_method_list] # api internally clear this field after a call
+#             result = self._api.call()
+#             if not self._is_response_valid(result, request_callers):
+#                 try_cnt += 1
+#                 logger.log('Server seems to be busy or offline - try again - {}/{}'.format(try_cnt, max_retry), 'red')
+
+#                 if try_cnt >= max_retry:
+#                     raise ServerBusyOrOfflineException()
+#                 sleep(1)
+#             else:
+#                 break
+
+#         self.last_api_request_time = request_timestamp
+#         return result
+
+#     def login(self, provider, username, password):
+#         return self._api.login(provider, username, password)
+
+#     # fallback
+#     def __getattr__(self, func):
+#         DEFAULT_ATTRS = ['_position_lat', '_position_lng', '_auth_provider', '_api_endpoint', 'set_position', 'get_position']
+#         if func not in DEFAULT_ATTRS:
+#             self.request_callers.append(func)
+#         return getattr(self._api, func)
+
+#     def throttle_sleep(self):
+#         now_milliseconds = time.time() * 1000
+#         required_delay_between_requests = 1000 / self.requests_per_seconds
+
+#         difference = now_milliseconds - (self.last_api_request_time if self.last_api_request_time else 0)
+
+#         if (self.last_api_request_time != None and difference < required_delay_between_requests):
+#             sleep_time = required_delay_between_requests - difference
+#             time.sleep(sleep_time / 1000)
+
+#         return now_milliseconds
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,19 @@
 # __init__.py
 from mock import MagicMock
 
-from pgoapi import PGoApi
 from pokemongo_bot.api_wrapper import ApiWrapper
 from pokemongo_bot import PokemonGoBot
 
 class FakeApi(ApiWrapper):
     def __init__(self, return_value=None):
-        super(FakeApi, self).__init__(PGoApi())
-        self._api.call = MagicMock(return_value=return_value)
+        super(FakeApi, self).__init__()
+        self._call = MagicMock(return_value=return_value)
 
     def _can_call(self):
         return True
 
     def setApiReturnValue(self, value):
-        self._api.call.return_value = value
+        self._call.return_value = value
 
 
 class FakeBot(PokemonGoBot):
