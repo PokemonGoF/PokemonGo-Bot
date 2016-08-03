486f7aaf4c55dd5c227357165c0bd131fd68dbab
Junaos <Junaos@junaos.com>
2016-08-03 02:00:42 -0400

    Fixed bad conversion in polyline_generator_tester

--- pokemongo_bot/walkers/polyline_generator_tester.py
+++ pokemongo_bot/walkers/polyline_generator_tester.py
@@ -10,10 +10,10 @@ from .polyline_generator import Polyline
 
 a = Polyline((47.1706378, 8.5167405), (47.1700271, 8.518072999999998), 100)
 print(a.points)
-print(('Walking polyline: ', a.polyline))
-print(('Encoded level: ','B'*len(a.points)))
-print(('Initialted with speed: ', a.speed, 'm/s'))
-print(('Walking time: ', ceil(sum([haversine.haversine(*x)*1000/a.speed for x in a.walk_steps()])), ' sec.'))
+print('Walking polyline: ', a.polyline)
+print('Encoded level: ','B'*len(a.points))
+print('Initialted with speed: ', a.speed, 'm/s')
+print('Walking time: ', ceil(sum([haversine.haversine(*x)*1000/a.speed for x in a.walk_steps()])), ' sec.')
 generated_polyline = []
 while a.points[-1] != a.get_pos()[0]:
     pos = a.get_pos()
@@ -23,5 +23,5 @@ while a.points[-1] != a.get_pos()[0]:
 else:
     print("We have reached our destination.")
 print(polyline.encode(generated_polyline))
-print(('Encoded level: ','B'*len(generated_polyline)))
-print(('Verify at: ', 'https://developers.google.com/maps/documentation/utilities/polylineutility'))
+print('Encoded level: ','B'*len(generated_polyline))
+print('Verify at: ', 'https://developers.google.com/maps/documentation/utilities/polylineutility')
