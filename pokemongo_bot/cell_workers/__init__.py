4a54e55bd6166ff601bc993e12edf091e4150498
Nick Hudkins <nick@nickhudkins.com>
2016-08-03 15:35:36 -0400

    :shirt: cleanup

--- pokemongo_bot/cell_workers/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pokemon_catch_worker import PokemonCatchWorker
-from seen_fort_worker import SeenFortWorker
-from move_to_fort_worker import MoveToFortWorker
-from initial_transfer_worker import InitialTransferWorker
-from evolve_all_worker import EvolveAllWorker
--- pokemongo_bot/polyline_walker/__init__.py
+++
@@ -1,1 +0,0 @@
-from polyline_walker import PolylineWalker
