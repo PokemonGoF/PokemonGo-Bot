f0a6d9d8a4aa77a6de78eb435f1c02178b66b3e9
Peter Bonanni <peter@ironclad.mobi>
2016-08-03 14:25:55 -0500

    Fix Typo in unexpected_response_retry
    fixes #2525 #2523

--- pokemongo_bot/api_wrapper.py
+++ pokemongo_bot/api_wrapper.py
@@ -117,7 +117,7 @@ class ApiRequest(PGoApiRequest):
                 continue # skip response checking
 
             if should_unexpected_response_retry:
-                unexpected_reponse_retry += 1
+                unexpected_response_retry += 1
                 if unexpected_response_retry >= 5:
                     logger.log('Server is not responding correctly to our requests.  Waiting for 30 seconds to reconnect.', 'red')
                     sleep(30)
