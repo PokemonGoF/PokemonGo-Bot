51869d8b778ac6dd6d391d3713903d7c6a255507
z4ppy bibicy <z4ppy.bbc@gmail.com>
2016-08-03 16:26:39 +0000

    Fix log & speed

--- pokemongo_bot/cell_workers/hunt_nearby_pokemon.py
+++ pokemongo_bot/cell_workers/hunt_nearby_pokemon.py
@@ -17,9 +17,11 @@ class HuntNearbyPokemon(BaseTask):
         self.stop_min_anyball = self.config.get("stop_min_anyball", 20)
         self.stop_max_time = self.config.get("stop_max_time", 60) * 60
         self.show_radar = self.config.get("show_radar", True)
+        self.fix_speed = self.config.get("fix_speed", True)
         self.ptr = 0
         self.direction = 1
         self.start_search = 0
+        self.last_step_time = 0
 
     def work(self):
 
@@ -39,9 +41,8 @@ class HuntNearbyPokemon(BaseTask):
                 for poke in self.bot.cell['nearby_pokemons']:
                     pokemon_num = int(poke['pokemon_id']) - 1
                     pokemon_name = self.bot.pokemon_list[int(pokemon_num)]['Name']
-                    strpoke = strpoke + "{}:{}({}) / ".format(pokemon_name, str(poke['encounter_id'])[:5],
-                                                     int(poke['distance_in_meters']) / 10)
-                logger.log('Pokemon in radar : {}'.format(strpoke))
+                    strpoke = strpoke + "{}:{} / ".format(pokemon_name, str(poke['encounter_id'])[:3])
+                logger.log(' Radar : {}'.format(strpoke))
 
         items_stock = self.bot.current_inventory()
         anyball_count = items_stock[1] + items_stock[2] + items_stock[3]
@@ -52,20 +53,18 @@ class HuntNearbyPokemon(BaseTask):
                 logger.log("No enought balls, stopping search :/")
                 self._end_search()
             elif ((time.time() - self.start_search) < self.stop_max_time):
-                logger.log("Searching at {},{}".format(self.bot.position[0], self.bot.position[1]))
+                #logger.log("Searching at {},{}".format(self.bot.position[0], self.bot.position[1]))
                 if self._continue_search():
                     return WorkerResult.RUNNING
                 else:
-                    logger.log("End search pokemon")
                     self._end_search()
             elif (free_pokemon_slot < 1):
                 logger.log("Not enought space for new pokemon :(")
                 self._end_search()
             else:
-                logger.log("End search pokemon")
                 self._end_search()
         elif 'nearby_pokemons' in self.bot.cell and anyball_count >= self.start_min_anyball and free_pokemon_slot > 0 and self._should_hunt(self.bot.cell['nearby_pokemons']):
-           logger.log("Start search pokemon \o/")
+           #logger.log("start search pokemon !")
            self._start_search()
            return WorkerResult.RUNNING
 
@@ -79,10 +78,24 @@ class HuntNearbyPokemon(BaseTask):
 
     def _continue_search(self):
         point = self.points[self.ptr]
+        # steroid if needed
+        if not self.fix_speed or self.last_step_time == 0:
+            fix_speed = self.bot.config.walk
+        else:
+            #trick/ugly, need fix StepWalker
+            fix_speed = min(40, self.bot.config.walk * min(10, max(1, time.time() - self.last_step_time)))
+            #logger.log("fixed speed={} (time={})".format(fix_speed, time.time() - self.last_step_time))
 
         step_walker = StepWalker(
             self.bot,
-            self.bot.config.walk,
+            fix_speed,
+            point['lat'],
+            point['lng']
+        )
+
+        dist = distance(
+            self.bot.api._position_lat,
+            self.bot.api._position_lng,
             point['lat'],
             point['lng']
         )
@@ -90,6 +103,7 @@ class HuntNearbyPokemon(BaseTask):
         if step_walker.step():
             step_walker = None
 
+        self.last_step_time = time.time()
         if distance(
                     self.bot.api._position_lat,
                     self.bot.api._position_lng,
@@ -100,12 +114,17 @@ class HuntNearbyPokemon(BaseTask):
                 return False
             if len(self.points) != 1:
                 self.ptr += self.direction
-                logger.log("Next point! {}/{}".format(self.ptr, len(self.points)))
-
+                dest = self.points[self.ptr]
+                logger.log(" [{}/{} ({}min)] Search from {} to {}".format(self.ptr, len(self.points),
+                                                        int((time.time() - self.start_search) / 60),
+                                                        str((self.bot.api._position_lat,self.bot.api._position_lng)),
+                                                        str((dest['lat'],dest['lng']))))
         return True
 
     def _end_search(self):
+        logger.log("[{}/{}] Stop hunting ({}min)".format(self.ptr, len(self.points), int((time.time() - self.start_search) / 60)))
         self.start_search = 0
+        self.last_step_time = 0
 
     def _should_hunt(self, nearby):
         vip_detected = False
@@ -114,15 +133,15 @@ class HuntNearbyPokemon(BaseTask):
                 pokemon_num = int(poke['pokemon_id']) - 1
                 pokemon_name = self.bot.pokemon_list[int(pokemon_num)]['Name']
                 if self.check_vip_pokemon(pokemon_name):
-                    logger.log("VIP pokemon detected ! ({})".format(pokemon_name))
+                    logger.log(" VIP pokemon detected \o/ ({})".format(pokemon_name))
                     vip_detected = True
 
         if len(nearby) >= self.start_count_pokemon:
-            logger.log("Some pokemons here :)")
             return True
 
         return vip_detected
 
+    # for real spiral :)
     def magic_x(self, x):
         return 5 * x * math.cos(x)
 
@@ -136,7 +155,7 @@ class HuntNearbyPokemon(BaseTask):
         cycle = int(search_range / start_step * 2)
         coords = [{'lat': starting_lat, 'lng': starting_lng}]
         for i in range(1, cycle):
-            logger.log("step {}! {}".format(i, current_step))
+            #logger.log("step {}! {}".format(i, current_step))
             mlat = mlat + current_step
             lat, lng = merc2coord((mlat, mlon))
             coords.append({'lat': lat, 'lng': lng})
