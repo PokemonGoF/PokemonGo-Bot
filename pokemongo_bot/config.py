35a815ed619ed24e5cc8415a529b8cb8fef6fa8c
M.K <shahrdar@localhost.localdomain>
2016-08-06 14:26:33 +0900

    Worked around with nested config

--- pokemongo_bot/config.py
+++ pokemongo_bot/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     _instance = None
-    _initialized = False
+    config = None
     def __new__(class_, *args, **kwargs):
         if not isinstance(class_._instance, class_):
             class_._instance = object.__new__(class_, *args, **kwargs)
@@ -9,20 +9,57 @@ class Config(object):
     def __init__(self):
         pass
 
-    def initialize(self, config):
-        self.config = config
-        Config._initialized = True
-            
-    def __getattr__(self, name):
-        if Config._initialized:
-            if hasattr(self, 'config'):
-                return getattr(self.config, name)
+    @staticmethod
+    def initialize(config):
+        Config.config = config
+          
+    @staticmethod
+    def __getattr__(name):
+        if None != Config.config:
+            return getattr(Config.config, name)
         return None
 
-def get_config(attr, default, config=Config()):
-    if Config._initialized and  hasattr(config, attr):
-        ret = getattr(config, attr)
+
+def _recursive_getattr(obj, attrNames):
+    if 0 == len(attrNames):
+        return obj
     else:
+        if type(obj) == dict and attrNames[0] in obj:
+            return _recursive_getattr(obj[attrNames[0]], attrNames[1:])
+        else:
+            return None
+
+
+def get_config(attr, default):
+    '''
+    Utility for getting config value.
+    Instead of writing
+
+    if 'attr1' in Config.config.dic and 'attr2' in Config.config.dic['attr1']:
+        value = Config.config.dic['attr1']['attr2']
+    else:
+        value = default
+
+    you can simply write
+
+    value = get_config('dic.attr1.attr2', default)
+
+    Array indexing is not supported yet.
+    TODO: Maybe add memoize???
+          Be careful with changin the conf values in the case...
+
+    '''
+    attr_ar = attr.split('.')
+    if None != Config.config and hasattr(Config.config, attr_ar[0]):
+        obj = getattr(Config.config, attr_ar[0])
+        if 1 == len(attr_ar):
+            return obj
+        else:
+            ret = _recursive_getattr(obj, attr_ar[1:])
+    else:
+        ret = None
+
+    if None == ret:
         # logger.log("Config for %s not found, using default = %s"%(attr, repr(default)))
         ret = default
     return ret
--- pokemongo_bot/test/config_singleton_test.py
+++ pokemongo_bot/test/config_singleton_test.py
@@ -10,20 +10,32 @@ class ConfigSingletonTestCase(unittest.TestCase):
 
         obj = AttrHolder()
         obj.val = 1
+        obj.d = {'apple':'is delicious', 'banana':{'is':'good'}}
 
         assert 0 == get_config('val', 0)
 
         config1 = Config()
-        assert 0 == get_config('val', 0)
+        assert 0 == get_config('val', 0) # not initialized yet.
 
         config2 = Config()
         config2.initialize(obj)
-        assert 1 == get_config('val', 0)
+        assert 1 == get_config('val', 0) # initialized.
 
         obj.val = 2
         config1.initialize(obj)
-        assert 2 == get_config('val', 0)
+        assert 2 == get_config('val', 0) # value changed, and other conf instance gets the canged value.
 
         config3 = Config()
-        obj.val = 3
-        assert 3 == get_config('val', 0)
+        config3.config.val = 3 # value changed through an instance
+        assert 3 == get_config('val', 0) # 
+
+        assert 'is delicious' == get_config('d.apple', 'WRONG') # nested attribute test
+
+        assert 'good' == get_config('d.banana.is', 'WRONG') # another nested attribute test
+        
+        assert 'WRONG' == get_config('d.pineapple.does.not.exist', 'WRONG') # some other nested attribute test
+
+        assert 3 == config2.config.val # make sure direct access works as it used to
+
+        assert 'good' == config2.config.d['banana']['is'] # same as above
+
