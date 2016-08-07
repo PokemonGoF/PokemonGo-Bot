c44f895abf025b3a36a749908229ae3db1438656
mjmadsen <mattjmadsen@gmail.com>
2016-08-07 15:51:45 -0500

    Changed initialization location for "bot"
    We use bot in main exception on 128

--- pokecli.py
+++ pokecli.py
@@ -51,9 +51,10 @@ logging.basicConfig(
 logger = logging.getLogger('cli')
 logger.setLevel(logging.INFO)
 
+bot = False
+
 def main():
     try:
-        bot = False
         logger.info('PokemonGO Bot v1.0')
         sys.stdout = codecs.getwriter('utf8')(sys.stdout)
         sys.stderr = codecs.getwriter('utf8')(sys.stderr)
