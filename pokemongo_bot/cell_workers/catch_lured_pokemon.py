d5bb09fd758e779b8eee859d8374f0d198e58abb
David Kim <dkim1000@msn.com>
2016-08-07 06:32:43 -0400

    Fix for utf8 encoding when catching lured pokemon (#2720)
    * Fixing lure pokestop encoding
    
    * fixing lure encoding

--- pokemongo_bot/cell_workers/catch_lured_pokemon.py
+++ pokemongo_bot/cell_workers/catch_lured_pokemon.py
@@ -24,7 +24,7 @@ class CatchLuredPokemon(BaseTask):
         details = fort_details(self.bot, fort_id=fort['id'],
                               latitude=fort['latitude'],
                               longitude=fort['longitude'])
-        fort_name = details.get('name', 'Unknown').encode('utf8', 'replace')
+        fort_name = details.get('name', 'Unknown')
 
         encounter_id = fort.get('lure_info', {}).get('encounter_id', None)
 
@@ -32,7 +32,7 @@ class CatchLuredPokemon(BaseTask):
             result = {
                 'encounter_id': encounter_id,
                 'fort_id': fort['id'],
-                'fort_name': fort_name,
+                'fort_name': u"{}".format(fort_name),
                 'latitude': fort['latitude'],
                 'longitude': fort['longitude']
             }
