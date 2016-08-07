eca3be022b38328770375414c2a8571da7899e1c
mhdasding <adler133738296@googlemail.com>
2016-08-07 21:27:08 +0200

    remove log

--- pokemongo_bot/cell_workers/nickname_pokemon.py
+++ pokemongo_bot/cell_workers/nickname_pokemon.py
@@ -19,8 +19,8 @@ class NicknamePokemon(BaseTask):
             if os.path.isfile(fn):
                 self.pokemon_list = json.load(open(fn))
             else:
+                # fallback to en
                 self.pokemon_list = self.bot.pokemon_list
-                logger.log('Locale {} not supported, fallback to en'.format(locale), 'red')
 
     def work(self):
         try:
