6f5149083331de71ed53ec7addd834620a03bfd5
Simon Shi <simonsmh@gmail.com>
2016-08-09 22:32:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 pokebotpath=$(pwd)
-filename=$*
-cd $pokebotpath
-if [ ! -f ./configs/"$filename" ]
-then
-echo "There's no ./configs/"$filename" file. use setup.sh -config to creat one."
+filename=""
+if [ ! -z $1 ]; then
+filename=$1
 else
+filename="./configs/config.json"
+if [ ! -f "$filename" ]
+then
+echo "There's no "$filename" file. use setup.sh -config to creat one."
+fi
+fi
+
 while true
 do
 cd $pokebotpath
-python pokecli.py -cf ./configs/"$filename"
+python pokecli.py -cf $filename
 read -p "Press any button or wait 20 seconds." -r -s -n1 -t 20
 echo `date`"Pokebot"$*" Stopped." 
 done
-fi
 exit 0
