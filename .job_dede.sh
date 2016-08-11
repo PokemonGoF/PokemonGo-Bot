7c2ffc16eb51b9f1a885eebd0c8e91b5f89e017a
Shane Jiang <jshangmin2011@gmail.com>
2016-08-10 20:33:25 -0400

    more bots
    **

---
+++ .job_dede.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while true
+do
+    python pokecli.py -cf ./configs/config.json.deidei
+done
---
+++ .job_xiangping.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while true
+do
+    gtimeout 3600 python pokecli.py -cf ./configs/config.json.xiangping
+done
