58a2b0d8fe803a37df59738e59bfcebf867c6b0b
cmezh <dmovodov@gmail.com>
2016-08-12 07:12:06 +0700

    Revert "Modify pokemon_catch_worker.py to use Inventory class"
    This reverts commit 5b6e4d39bccbae6a825274080727bd7ef62b6d98.

--- pokemongo_bot/inventory.py
+++ pokemongo_bot/inventory.py
@@ -110,9 +110,6 @@ class Items(_BaseInventoryComponent):
     def count_for(self, item_id):
         return self._data[item_id]['count']
 
-    def decrement_count(self, item_id):
-        self._data[item_id]["count"] -= 1
-
 
 class Pokemons(_BaseInventoryComponent):
     TYPE = 'pokemon_data'
