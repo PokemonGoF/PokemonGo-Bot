0af3dc0bed9d90006d5eed676c8eee4c1fcc981e
cmezh <dmovodov@gmail.com>
2016-08-12 07:09:53 +0700

    Revert "Fix KeyError in inventory.py"
    This reverts commit ed2769c51820381044332f9e95e759bda6dc587e.

--- pokemongo_bot/inventory.py
+++ pokemongo_bot/inventory.py
@@ -108,10 +108,7 @@ class Items(_BaseInventoryComponent):
     STATIC_DATA_FILE = os.path.join(_base_dir, 'data', 'items.json')
 
     def count_for(self, item_id):
-        try:
-            return self._data[item_id]['count']
-        except KeyError:
-            return 0
+        return self._data[item_id]['count']
 
     def decrement_count(self, item_id):
         self._data[item_id]["count"] -= 1
