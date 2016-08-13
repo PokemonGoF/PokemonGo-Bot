9de16e742e58dfce03a0dbfea3b9e17fe375dfea
M.K <shahrdar@localhost.localdomain>
2016-08-14 00:29:37 +0900

    get the CI pass

--- pokemongo_bot/test/step_walker_test.py
+++ pokemongo_bot/test/step_walker_test.py
@@ -1,8 +1,8 @@
 import unittest
 from mock import MagicMock, patch
 
-from pokemongo_bot.walkers.step_walker import StepWalker
 from pokemongo_bot.cell_workers.utils import float_equal
+from pokemongo_bot.walkers.step_walker import StepWalker
 
 NORMALIZED_LAT_LNG_DISTANCE_STEP = 6.3593e-6
 
