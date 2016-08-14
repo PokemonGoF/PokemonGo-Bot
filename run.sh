15692c2b5b0af2f9c2f6328443e6b75848827e19
nivong <nivong@users.noreply.github.com>
2016-08-14 15:43:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,12 @@ echo "There's no "$filename" file. Please use ./setup.sh -c to creat one."
 fi
 while true
 do
+echo "Checking for updates...."
+git pull
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+read -p "WARNING: Verify if the Config.json file got updated. If Yes, check if your modifications are still valid before proceeding. Waiting 20 seconds so you can verify and stop the bot.
+" -r -s -n1 -t 20
 python pokecli.py -cf $filename
 echo `date`" Pokebot "$*" Stopped." 
 read -p "Press any button or wait 20 seconds to continue.
