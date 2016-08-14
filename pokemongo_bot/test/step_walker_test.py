23f2bc941d442058fa8fa26120cf477ac165906f
M.K <shahrdar@localhost.localdomain>
2016-08-14 12:16:38 +0900

    fix test

--- pokemongo_bot/test/step_walker_test.py
+++ pokemongo_bot/test/step_walker_test.py
@@ -1,15 +1,15 @@
 import unittest
 from mock import MagicMock, patch
 
-from pokemongo_bot.cell_workers.utils import float_equal
 from pokemongo_bot.walkers.step_walker import StepWalker
+from pokemongo_bot.cell_workers.utils import float_equal
 
 NORMALIZED_LAT_LNG_DISTANCE_STEP = 6.3593e-6
 
 class TestStepWalker(unittest.TestCase):
     def setUp(self):
-        self.patcherSleep = patch('pokemongo_bot.step_walker.sleep')
-        self.patcherRandomLat = patch('pokemongo_bot.step_walker.random_lat_long_delta', return_value=0)
+        self.patcherSleep = patch('pokemongo_bot.walkers.step_walker.sleep')
+        self.patcherRandomLat = patch('pokemongo_bot.walkers.step_walker.random_lat_long_delta', return_value=0)
         self.patcherSleep.start()
         self.patcherRandomLat.start()
 
