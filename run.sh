abf2b80d2a62d2dd6ea31dde65bc626f07e8826a
nivong <nivong@users.noreply.github.com>
2016-08-14 15:52:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,8 +19,6 @@ git submodule update --init --recursive
 git submodule foreach git pull origin master
 read -p "WARNING: Verify if the Config.json file got updated. If Yes, check if your modifications are still valid before proceeding. Waiting 20 seconds so you can verify and stop the bot.
 " -r -s -n1 -t 20
-done
-exit 0
 python pokecli.py -cf $filename
 echo `date`" Pokebot "$*" Stopped." 
 read -p "Press any button or wait 20 seconds to continue.
