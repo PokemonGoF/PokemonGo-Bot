1b14c55fc10d0872a8626d9e1859babf37af08d1
Vince <vince@shystudios.co.uk>
2016-08-15 22:04:13 +0100

    indentation

--- pokemongo_bot/cell_workers/catch_pokemon.py
+++ pokemongo_bot/cell_workers/catch_pokemon.py
@@ -5,17 +5,17 @@ from pokemongo_bot.cell_workers import CatchVisiblePokemon, CatchLuredPokemon
 
 
 class CatchPokemon(BaseTask):
-	SUPPORTED_TASK_API_VERSION = 1
+    SUPPORTED_TASK_API_VERSION = 1
 
-	def initialize(self):
-		self.catch_workers = []
-		if self.config['catch_visible_pokemon']:
-			self.catch_workers.append(CatchVisiblePokemon(self.bot, self.config))
-		if self.config['catch_lured_pokemon']:
-			self.catch_workers.append(CatchLuredPokemon(self.bot, self.config))
+    def initialize(self):
+        self.catch_workers = []
+        if self.config['catch_visible_pokemon']:
+            self.catch_workers.append(CatchVisiblePokemon(self.bot, self.config))
+        if self.config['catch_lured_pokemon']:
+            self.catch_workers.append(CatchLuredPokemon(self.bot, self.config))
 
-	def work(self):
-		for cw in self.catch_workers:
-			if cw.work() == WorkerResult.RUNNING:
-				return WorkerResult.RUNNING
-		return WorkerResult.SUCCESS
+    def work(self):
+        for cw in self.catch_workers:
+            if cw.work() == WorkerResult.RUNNING:
+                return WorkerResult.RUNNING
+        return WorkerResult.SUCCESS
