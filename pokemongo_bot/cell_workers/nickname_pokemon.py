b1323b956ccf491aef53c74fb85b58568493fbd8
DeXtroTip <DeXtroTip@users.noreply.github.com>
2016-08-15 02:37:29 +0100

    More configs on UpdateLiveInventory

--- pokemongo_bot/cell_workers/nickname_pokemon.py
+++ pokemongo_bot/cell_workers/nickname_pokemon.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import os
 import json
 from pokemongo_bot.base_task import BaseTask
--- pokemongo_bot/cell_workers/update_live_inventory.py
+++ pokemongo_bot/cell_workers/update_live_inventory.py
@@ -11,15 +11,13 @@ class UpdateLiveInventory(BaseTask):
     """
     Periodically displays the user inventory in the terminal.
 
-    Fetching some stats requires making API calls. If you're concerned about the amount of calls
-    your bot is making, don't enable this worker.
-
     Example config :
     {
         "type": "UpdateLiveInventory",
         "config": {
           "enabled": true,
           "min_interval": 120,
+          "show_all_multiple_lines": false,
           "items": ["space_info", "pokeballs", "greatballs", "ultraballs", "razzberries", "luckyegg"]
         }
     }
@@ -27,6 +25,8 @@ class UpdateLiveInventory(BaseTask):
     min_interval : The minimum interval at which the stats are displayed,
                    in seconds (defaults to 120 seconds).
                    The update interval cannot be accurate as workers run synchronously.
+    show_all_multiple_lines : Logs all items on inventory using multiple lines.
+                              Ignores configuration of 'items' 
     items : An array of items to display and their display order (implicitly),
             see available items below (defaults to []).
 
@@ -58,12 +58,26 @@ class UpdateLiveInventory(BaseTask):
     def initialize(self):
         self.next_update = None
         self.min_interval = self.config.get('min_interval', 120)
+        self.show_all_multiple_lines = self.config.get('show_all_multiple_lines', False)
         self.displayed_items = self.config.get('items', [])
 
-        self.bot.event_manager.register_event('show_inventory', parameters=('items',))
+        if self.show_all_multiple_lines:
+            self.bot.event_manager.register_event('show_inventory')
+        else:
+            self.bot.event_manager.register_event('show_inventory', parameters=('items',))
+
 
     def work(self):
+        """
+        Displays the items if necessary.
+        :return: Always returns WorkerResult.SUCCESS.
+        :rtype: WorkerResult
+        """
         if not self.should_print():
+            return WorkerResult.SUCCESS
+        self.inventory = inventory.items()
+        if self.show_all_multiple_lines:
+            self.print_all()
             return WorkerResult.SUCCESS
         line = self.get_inventory_line()
         if not line:
@@ -72,12 +86,29 @@ class UpdateLiveInventory(BaseTask):
         return WorkerResult.SUCCESS
 
     def should_print(self):
+        """
+        Returns a value indicating whether the items should be displayed.
+        :return: True if the stats should be displayed; otherwise, False.
+        :rtype: bool
+        """
         return self.next_update is None or datetime.now() >= self.next_update
 
     def compute_next_update(self):
+        """
+        Computes the next update datetime based on the minimum update interval.
+        :return: Nothing.
+        :rtype: None
+        """
         self.next_update = datetime.now() + timedelta(seconds=self.min_interval)
 
     def print_inv(self, items):
+        """
+        Logs the items into the terminal using an event.
+        :param items: The items to display.
+        :type items: string
+        :return: Nothing.
+        :rtype: None
+        """
         self.emit_event(
             'show_inventory',
             formatted="{items}",
@@ -89,8 +120,11 @@ class UpdateLiveInventory(BaseTask):
 
 
     def get_inventory_line(self):
-        self.inventory = inventory.items()
-
+        """
+        Generates a items string according to the configuration.
+        :return: A string containing items and their count, ready to be displayed.
+        :rtype: string
+        """
         available_items = {
             'space_info': 'Items: {:,}/{:,}'.format(self.inventory.get_space_used(),
                                                     self.inventory.get_space_used() + self.inventory.get_space_left()),
@@ -116,10 +150,97 @@ class UpdateLiveInventory(BaseTask):
         }
 
         def get_item(item):
+            """
+            Fetches a item string from the available items dictionary.
+            :param item: The item name.
+            :type item: string
+            :return: The generated item string.
+            :rtype: string
+            :raise: ConfigException: When the provided item string isn't in the available items
+            dictionary.
+            """
             if item not in available_items:
                 raise ConfigException("item '{}' isn't available for displaying".format(item))
             return available_items[item]
 
         line = ' | '.join(map(get_item, self.displayed_items))
-
         return line
+
+    def print_all(self):
+        """
+        Logs the items into the terminal using an event.
+        It logs using multiple lines and logs all items.
+        :return: Nothing.
+        :rtype: None
+        """
+        self.emit_event(
+                'show_inventory',
+                formatted='Items: {items_count}/{item_bag}',
+                data={
+                    'items_count': self.inventory.get_space_used(),
+                    'item_bag': self.inventory.get_space_used() + self.inventory.get_space_left()
+                }
+            )
+
+        self.emit_event(
+                'show_inventory',
+                formatted='PokeBalls: {pokeballs} | GreatBalls: {greatballs} | UltraBalls: {ultraballs} | MasterBalls: {masterballs}',
+                data={
+                    'pokeballs': str(self.inventory.get(1).count),
+                    'greatballs': str(self.inventory.get(2).count),
+                    'ultraballs': str(self.inventory.get(3).count),
+                    'masterballs': str(self.inventory.get(4).count)
+                }
+            )
+
+        self.emit_event(
+                'show_inventory',
+                formatted='RazzBerries: {razzberries} | BlukBerries: {blukberries} | NanabBerries: {nanabberries}',
+                data={
+                    'razzberries': self.inventory.get(701).count,
+                    'blukberries': self.inventory.get(702).count,
+                    'nanabberries': self.inventory.get(703).count
+                }
+            )
+
+        self.emit_event(
+                'show_inventory',
+                formatted='LuckyEgg: {luckyegg} | Incubator: {incubator} | TroyDisk: {troydisk}',
+                data={
+                    'luckyegg': self.inventory.get(301).count,
+                    'incubator': self.inventory.get(902).count,
+                    'troydisk': self.inventory.get(501).count
+                }
+            )
+
+        self.emit_event(
+                'show_inventory',
+                formatted='Potion: {potion} | SuperPotion: {superpotion} | HyperPotion: {hyperpotion} | MaxPotion: {maxpotion}',
+                data={
+                    'potion': self.inventory.get(101).count,
+                    'superpotion': self.inventory.get(102).count,
+                    'hyperpotion': self.inventory.get(103).count,
+                    'maxpotion': self.inventory.get(104).count
+                }
+            )
+
+        self.emit_event(
+                'show_inventory',
+                formatted='Incense: {incense} | IncenseSpicy: {incensespicy} | IncenseCool: {incensecool}',
+                data={
+                    'incense': self.inventory.get(401).count,
+                    'incensespicy': self.inventory.get(402).count,
+                    'incensecool': self.inventory.get(403).count
+                }
+            )
+           
+        self.emit_event(
+                'show_inventory',
+                formatted='Revive: {revive} | MaxRevive: {maxrevive}',
+                data={
+                    'revive': self.inventory.get(201).count,
+                    'maxrevive': self.inventory.get(202).count
+                }
+           )
+
+        self.compute_next_update()
