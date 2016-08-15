0408e5d893b5c1c306d1be4807a03be75b43cbce
Simon Shi <simonsmh@gmail.com>
2016-08-15 09:47:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,14 @@ filename="./configs/config.json"
 fi
 cd $pokebotpath
 source bin/activate
+git fetch -a
+if [ "1" == $(git branch -vv |grep -c "* dev") ] && [ $(git log --pretty=format:"%h" -1) != $(git log --pretty=format:"%h" -1 origin/dev) ]
+then
+echo "on dev update"
+elif [ "1" == $(git branch -vv |grep -c "* master") ] && [ $(git log --pretty=format:"%h" -1) != $(git log --pretty=format:"%h" -1 origin/master) ]
+then 
+echo "on master update"
+fi
 if [ ! -f "$filename" ]; then
 echo "There's no "$filename" file. Please use ./setup.sh -c to creat one."
 fi
