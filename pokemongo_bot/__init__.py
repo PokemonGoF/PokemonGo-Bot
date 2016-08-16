130f7c019ef9f984720083c2e95eb583038b3a7a
Simon Shi <simonsmh@gmail.com>
2016-08-16 18:54:06 +0800

    try to fix system platform in init.py
    Fix for other system we don't know but should run without problem.

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -678,14 +678,14 @@ class PokemonGoBot(object):
         )
 
     def get_encryption_lib(self):
-        if _platform == "linux" or _platform == "linux2" or _platform == "darwin" or _platform == "freebsd10":
-            file_name = 'encrypt.so'
-        elif _platform == "Windows" or _platform == "win32":
+        if _platform == "Windows" or _platform == "win32":
             # Check if we are on 32 or 64 bit
             if sys.maxsize > 2**32:
                 file_name = 'encrypt_64.dll'
             else:
                 file_name = 'encrypt.dll'
+        else:
+            file_name = 'encrypt.so'
 
         if self.config.encrypt_location == '':
             path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
