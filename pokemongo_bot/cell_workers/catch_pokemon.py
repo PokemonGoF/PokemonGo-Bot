18fd2afb5de9efc11209c6a1a9e471f27b6b8956
Ingwar Wirjawan <ingwar.wirjawan@gmail.com>
2016-08-18 01:20:12 +0800

    stop catching pokemon if we do not have pokeballs

--- pokemongo_bot/cell_workers/catch_pokemon.py
+++ pokemongo_bot/cell_workers/catch_pokemon.py
@@ -2,6 +2,11 @@
 from pokemongo_bot.base_task import BaseTask
 from pokemongo_bot.worker_result import WorkerResult
 from pokemongo_bot.cell_workers import CatchVisiblePokemon, CatchLuredPokemon
+from pokemongo_bot import inventory
+
+ITEM_POKEBALL = 1
+ITEM_GREATBALL = 2
+ITEM_ULTRABALL = 3
 
 
 class CatchPokemon(BaseTask):
@@ -15,6 +20,12 @@ class CatchPokemon(BaseTask):
             self.catch_workers.append(CatchLuredPokemon(self.bot, self.config))
 
     def work(self):
+
+        if sum([inventory.items().get(ball_id).count for ball_id in 
+            [ITEM_POKEBALL, ITEM_GREATBALL, ITEM_ULTRABALL]]) <= 0:
+            self.emit_event('no_pokeballs', formatted='No usable pokeballs found!')
+            return WorkerResult.ERROR
+
         for cw in self.catch_workers:
             if cw.work() == WorkerResult.RUNNING:
                 return WorkerResult.RUNNING
