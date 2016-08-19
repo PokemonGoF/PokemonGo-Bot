5ee2999ad47a0ba8d6df976b66ff1f9bf3720522
Simba Zhang <solderzzc@users.noreply.github.com>
2016-08-18 18:50:12 -0700

    Revert "Pokestops / Evolves / Transfers Logged to DB"

--- pokemongo_bot/cell_workers/migrations/evolve_log.py
+++
@@ -1,5 +0,0 @@
-from yoyo import step
-
-step(
-    "CREATE TABLE evolve_log (pokemon text, iv real, cp real, dated datetime DEFAULT CURRENT_TIMESTAMP)"
-)
--- pokemongo_bot/cell_workers/migrations/pokestop_log.py
+++
@@ -1,5 +0,0 @@
-from yoyo import step
-
-step(
-    "CREATE TABLE pokestop_log (pokestop text, exp real, items text, dated datetime DEFAULT CURRENT_TIMESTAMP)"
-)
