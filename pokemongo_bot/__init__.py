9b391c880f93b6cc6750b16350fafba6019ebe30
DeXtroTip <DeXtroTip@users.noreply.github.com>
2016-08-19 19:03:52 +0100

    Minor fix

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -387,7 +387,7 @@ class PokemonGoBot(Datastore):
         )
         self.event_manager.register_event(
             'next_egg_incubates',
-            parameters=('km_needed', 'distance_in_km',)
+            parameters=('km_needed', 'distance_in_km', 'eggs', 'eggs_inc')
         )
         self.event_manager.register_event('incubator_already_used')
         self.event_manager.register_event('egg_already_incubating')
