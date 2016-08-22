6d23cea7cb58c4a0ef4ca8be7fab1eb02be74b26
Ro <breezeqnz@gmail.com>
2016-08-22 17:19:30 -0400

    Update __init__.py

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -25,7 +25,7 @@ from event_manager import EventManager
 from human_behaviour import sleep
 from item_list import Item
 from metrics import Metrics
-from pokemongo_bot.event_handlers import LoggingHandler, SocketIoHandler, ColoredLoggingHandler
+from pokemongo_bot.event_handlers import LoggingHandler, SocketIoHandler, ColoredLoggingHandler, SocialHandler
 from pokemongo_bot.socketio_server.runner import SocketIoRunner
 from pokemongo_bot.websocket_remote_control import WebsocketRemoteControl
 from pokemongo_bot.base_dir import _base_dir
