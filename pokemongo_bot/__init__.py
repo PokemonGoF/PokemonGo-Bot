722e0d444f6f01fd9589d727951352add4a49dd4
Ro <breezeqnz@gmail.com>
2016-08-22 17:41:15 -0400

    Update __init__.py

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -117,7 +117,8 @@ class PokemonGoBot(Datastore):
             handlers.append(ColoredLoggingHandler())
         else:
             handlers.append(LoggingHandler())
-
+        if self.config.enable_social:
+            handlers.append(SocialHandler(self))
         if self.config.websocket_server_url:
             if self.config.websocket_start_embedded_server:
                 self.sio_runner = SocketIoRunner(self.config.websocket_server_url)
