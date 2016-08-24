9c87e8221142e6bd89e239996b1a2290a0c3c1af
Loris D'Antonio <Loris@Loris-MBP-2.lan>
2016-08-25 00:44:21 +0200

    Added some test to favorite_locations logic

--- tests/location_parser_test.py
+++ tests/location_parser_test.py
@@ -13,9 +13,15 @@ class TestLocationParser(unittest.TestCase):
         config = dict(
             test=False,
             location='Paris',
+            favorite_locations=[
+                {"name": "Paris", "coords": "23.3333,23.3333"},
+                {"name": "Sofia", "coords": "30.000,30.000"}
+            ],
             location_cache=False,
             username='Foobar',
         )
+        self.bot.alt = 8
+        self.bot.logger = MagicMock(return_value="")
         self.bot.updateConfig(config)
 
     def test_named_position(self):
@@ -23,6 +29,29 @@ class TestLocationParser(unittest.TestCase):
         self.bot.get_pos_by_name = MagicMock(return_value=position)
         self.bot._set_starting_position()
         self.assertEqual(self.bot.position, position)
+
+    def test_find_fav_location(self):
+        expected_position = (23.3333, 23.3333, 8)
+        actual_position = self.bot.get_pos_by_name(self.bot.config.location)
+        self.assertEqual(actual_position, expected_position)
+
+    def test_get_pos_by_fav_location(self):
+        expected_position = (23.3333,23.3333,8)
+        actual_position = self.bot._get_pos_by_fav_location(self.bot.config.location)
+        self.assertEqual(expected_position, actual_position)
+
+    def test_no_favorite_position(self):
+        result = self.bot._get_pos_by_fav_location("NOT_EXIST")
+        self.assertEqual(result,None)
+
+    def test_empty_favorite_position(self):
+        config = dict(
+            favorite_locations=[],
+        )
+        self.bot.updateConfig(config)
+
+        result = self.bot._get_pos_by_fav_location("Does not matter")
+        self.assertEqual(result,None)
 
     def test_named_position_utf8(self):
         position = (42, 42, 0)
