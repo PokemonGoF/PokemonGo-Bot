7bffde017572a7e40f21f8791af8264502a6cbc0
Alex Yao <alexyaoyang@gmail.com>
2016-08-24 15:31:35 -0700

    Allow alt in walker factory (#4698)

--- pokemongo_bot/walkers/walker_factory.py
+++ pokemongo_bot/walkers/walker_factory.py
@@ -1,15 +1,15 @@
 from pokemongo_bot.walkers.polyline_walker import PolylineWalker
 from pokemongo_bot.walkers.step_walker import StepWalker
 
-def walker_factory(name, bot, dest_lat, dest_lng, *args, **kwargs):
+def walker_factory(name, bot, dest_lat, dest_lng, dest_alt=None, *args, **kwargs):
     '''
     Charlie and the Walker Factory
     '''
     if 'StepWalker' == name:
-        ret = StepWalker(bot, dest_lat, dest_lng)
+        ret = StepWalker(bot, dest_lat, dest_lng, dest_alt)
     elif 'PolylineWalker' == name:
         try:
             ret = PolylineWalker(bot, dest_lat, dest_lng)
         except:
-            ret = StepWalker(bot, dest_lat, dest_lng)
+            ret = StepWalker(bot, dest_lat, dest_lng, dest_alt)
     return ret
