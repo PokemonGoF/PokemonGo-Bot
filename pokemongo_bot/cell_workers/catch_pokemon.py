fe7768c06162ba7d74c2fcad43cc5e19fe4ce4f2
Matt J Madsen <mattjmadsen@gmail.com>
2016-08-23 21:08:05 -0500

    Catcher improvements (#4640)
    * Encounter check + etc
    
    * Deleted catch_lured_pokemon
    
    * Deleted catch_visible_pokemon
    
    * No need to sort forts IMO
    
    * Removed capture_lured_* and capture_visible_*

--- pokemongo_bot/cell_workers/catch_pokemon.py
+++ pokemongo_bot/cell_workers/catch_pokemon.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import json
 import os
+import random
 
 from pokemongo_bot.base_task import BaseTask
 from pokemongo_bot.cell_workers.pokemon_catch_worker import PokemonCatchWorker
@@ -12,31 +13,41 @@ from pokemongo_bot import inventory
 from utils import fort_details, distance
 from pokemongo_bot.base_dir import _base_dir
 from pokemongo_bot.constants import Constants
-from pokemongo_bot.inventory import Pokemons, Pokemon, Attack
+
 
 class CatchPokemon(BaseTask):
     SUPPORTED_TASK_API_VERSION = 1
 
+    def initialize(self):
+        self.pokemon = []
+
     def work(self):
+        # make sure we have SOME balls
         if sum([inventory.items().get(ball.value).count for ball in 
             [Item.ITEM_POKE_BALL, Item.ITEM_GREAT_BALL, Item.ITEM_ULTRA_BALL]]) <= 0:
             return WorkerResult.ERROR
 
-        pokemon = []
-        if self.config.get('catch_visible_pokemon', True):
-            pokemon = self.get_visible_pokemon()
-        if self.config.get('catch_lured_pokemon', True):
-            pokemon += self.get_lured_pokemon()
+        # check if we have already loaded a list
+        if len(self.pokemon) <= 0:
+            # load available pokemon by config settings
+            if self.config.get('catch_visible_pokemon', True):
+                self.get_visible_pokemon()
+            if self.config.get('catch_lured_pokemon', True):
+                self.get_lured_pokemon()
 
-        num_pokemon = len(pokemon)
+            random.shuffle(self.pokemon)
+
+        num_pokemon = len(self.pokemon)
         if num_pokemon > 0:
-            pokemon = self.sort_pokemon(pokemon)
-            
-            if self.catch_pokemon(pokemon[0]) == WorkerResult.ERROR:
+            # try catching
+            if self.catch_pokemon(self.pokemon.pop()) == WorkerResult.ERROR:
+                # give up incase something went wrong in our catch worker (ran out of balls, etc)
                 return WorkerResult.ERROR
             elif num_pokemon > 1:
+                # we have more pokemon to catch
                 return WorkerResult.RUNNING
 
+        # all pokemon have been processed
         return WorkerResult.SUCCESS
 
     def get_visible_pokemon(self):
@@ -45,34 +56,35 @@ class CatchPokemon(BaseTask):
             pokemon_to_catch = self.bot.cell['catchable_pokemons']
 
             if len(pokemon_to_catch) > 0:
-    		    # Update web UI
-    		    user_web_catchable = os.path.join(_base_dir, 'web', 'catchable-{}.json'.format(self.bot.config.username))
-    		    for pokemon in pokemon_to_catch:
-    		        with open(user_web_catchable, 'w') as outfile:
-    		            json.dump(pokemon, outfile)
-    		        self.emit_event(
-    		            'catchable_pokemon',
-    		            level='debug',
-    		            data={
-    		                'pokemon_id': pokemon['pokemon_id'],
-    		                'spawn_point_id': pokemon['spawn_point_id'],
-    		                'encounter_id': pokemon['encounter_id'],
-    		                'latitude': pokemon['latitude'],
-    		                'longitude': pokemon['longitude'],
-    		                'expiration_timestamp_ms': pokemon['expiration_timestamp_ms'],
-    		                'pokemon_name': Pokemons.name_for(pokemon['pokemon_id']),
-    		            }
-    		        )
+    		user_web_catchable = os.path.join(_base_dir, 'web', 'catchable-{}.json'.format(self.bot.config.username))
+    		for pokemon in pokemon_to_catch:
+
+    	            # Update web UI
+    		    with open(user_web_catchable, 'w') as outfile:
+    		        json.dump(pokemon, outfile)
+
+    		    self.emit_event(
+    		        'catchable_pokemon',
+    		        level='debug',
+    		        data={
+    		            'pokemon_id': pokemon['pokemon_id'],
+    		            'spawn_point_id': pokemon['spawn_point_id'],
+    		            'encounter_id': pokemon['encounter_id'],
+    		            'latitude': pokemon['latitude'],
+    		            'longitude': pokemon['longitude'],
+    		            'expiration_timestamp_ms': pokemon['expiration_timestamp_ms'],
+    		        }
+    		    )
+
+                    self.add_pokemon(pokemon)
 
         if 'wild_pokemons' in self.bot.cell:
-            pokemon_to_catch += self.bot.cell['wild_pokemons']
-
-        return pokemon_to_catch
+            for pokemon in self.bot.cell['wild_pokemons']:
+                self.add_pokemon(pokemon)
 
     def get_lured_pokemon(self):
         forts_in_range = []
-        pokemon_to_catch = []
-        forts = self.bot.get_forts(order_by_distance=True)
+        forts = self.bot.get_forts(order_by_distance=False)
 
         if len(forts) == 0:
             return []
@@ -85,6 +97,7 @@ class CatchPokemon(BaseTask):
                 fort['longitude']
             )
 
+            # See if we have an encounter at this fort
             encounter_id = fort.get('lure_info', {}).get('encounter_id', None)
             if distance_to_fort < Constants.MAX_DISTANCE_FORT_IS_REACHABLE and encounter_id:
                 forts_in_range.append(fort)
@@ -97,14 +110,13 @@ class CatchPokemon(BaseTask):
             fort_name = details.get('name', 'Unknown')
             encounter_id = fort['lure_info']['encounter_id']
 
-            result = {
+            pokemon = {
                 'encounter_id': encounter_id,
                 'fort_id': fort['id'],
                 'fort_name': u"{}".format(fort_name),
                 'latitude': fort['latitude'],
                 'longitude': fort['longitude']
             }
-            pokemon_to_catch.append(result)
 
             self.emit_event(
                 'lured_pokemon_found',
@@ -112,20 +124,15 @@ class CatchPokemon(BaseTask):
                 formatted='Lured pokemon at fort {fort_name} ({fort_id})',
                 data=result
             )
-        return pokemon_to_catch
+
+            self.add_pokemon(pokemon)
+
+    def add_pokemon(self, pokemon):
+        if pokemon['encounter_id'] not in self.pokemon:
+            self.pokemon.append(pokemon)
 
     def catch_pokemon(self, pokemon):
         worker = PokemonCatchWorker(pokemon, self.bot, self.config)
         return_value = worker.work()
 
         return return_value
-        
-    def sort_pokemon(self, pokemon_list):
-        # Sort all by distance from current pos- eventually this should
-        # build graph & A* it
-        pokemon_list.sort(
-            key=
-            lambda x: distance(self.bot.position[0], self.bot.position[1], x['latitude'], x['longitude'])
-        )
-        
-        return pokemon_list
