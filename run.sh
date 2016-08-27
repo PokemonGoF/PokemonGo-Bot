9a9975169652c67d218a400756871bf88920a1a9
Fernando Figueroa <f.figueroa@silverhost.cl>
2016-08-27 17:02:48 -0300

    Added auth param to run.sh and auth generator to setup.sh (#4814)
    * Added auth.json to run.sh
    
    * Param auth for run.sh
    
    * Added auth generator for setup.sh
    
    * Modified config generator and added auth generator
    
    * Modified setup.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 pokebotpath=$(cd "$(dirname "$0")"; pwd)
-filename=""
+auth=""
+config=""
 if [ ! -z $1 ]; then
-filename=$1
+auth=$1
 else
-filename="./configs/config.json"
+auth="./configs/auth.json"
+fi
+if [ ! -z $2 ]; then
+config=$2
+else
+config="./configs/config.json"
 fi
 cd $pokebotpath
 source bin/activate
@@ -14,17 +20,20 @@ then
 echo "Branch dev have an update. Run ./setup.sh -u to update."
 sleep 2
 elif [ "1" == $(git branch -vv |grep -c "* master") ] && [ $(git log --pretty=format:"%h" -1) != $(git log --pretty=format:"%h" -1 origin/master) ]
-then 
+then
 echo "Branch master have an update. Run ./setup.sh -u to update."
 sleep 2
 fi
-if [ ! -f "$filename" ]; then
-echo "There's no "$filename" file. Please use ./setup.sh -c to creat one."
+if [ ! -f "$auth" ]; then
+echo "There's no auth file. Please use ./setup.sh -a to create one"
+fi
+if [ ! -f "$config" ]; then
+echo "There's no config file. Please use ./setup.sh -c to create one."
 fi
 while true
 do
-python pokecli.py -cf $filename
-echo `date`" Pokebot "$*" Stopped." 
+python pokecli.py -af $auth -cf $config
+echo `date`" Pokebot "$*" Stopped."
 read -p "Press any button or wait 20 seconds to continue.
 " -r -s -n1 -t 20
 done
