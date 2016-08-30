47e1ddd1e0a173cdade790981891a360d5cba42b
Simba Zhang <solderzzc@users.noreply.github.com>
2016-08-30 07:38:26 -0700

    Catch exception telegram.error.NetworkError. Fixs some pylint complain.

--- pokemongo_bot/cell_workers/telegram_task.py
+++ pokemongo_bot/cell_workers/telegram_task.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
-import telegram
 import os
 import logging
 import json
+import telegram
 from pokemongo_bot.base_task import BaseTask
 from pokemongo_bot.base_dir import _base_dir
 from pokemongo_bot.event_handlers import TelegramHandler
-
-from pprint import pprint
-import re
 
 class FileIOException(Exception):
     pass
@@ -22,15 +19,16 @@ class TelegramTask(BaseTask):
         if not self.enabled:
             return
         api_key = self.bot.config.telegram_token
-        if api_key == None:
+        if api_key is None:
             self.emit_event(
                 'config_error',
                 formatted='api_key not defined.'
             )
             return
         self.tbot = telegram.Bot(api_key)
-        if self.config.get('master',None):
-            self.bot.event_manager.add_handler(TelegramHandler(self.tbot,self.config.get('master',None),self.config.get('alert_catch')))
+        self.master = self.config.get('master', None)
+        if self.master:
+            self.bot.event_manager.add_handler(TelegramHandler(self.tbot, self.master, self.config.get('alert_catch')))
         try:
             self.update_id = self.tbot.getUpdates()[0].update_id
         except IndexError:
@@ -43,19 +41,16 @@ class TelegramTask(BaseTask):
             self.update_id = update.update_id+1
             if update.message:
                 self.bot.logger.info("message from {} ({}): {}".format(update.message.from_user.username, update.message.from_user.id, update.message.text))
-                if self.config.get('master',None) and self.config.get('master',None) not in [update.message.from_user.id, "@{}".format(update.message.from_user.username)]:
-                    self.emit_event( 
-                            'debug', 
-                            formatted="Master wrong: expecting {}, got {}({})".format(self.config.get('master',None), update.message.from_user.username, update.message.from_user.id))
+                if self.config.get('master', None) and self.config.get('master', None) not in [update.message.from_user.id, "@{}".format(update.message.from_user.username)]:
+                    self.emit_event(
+                        'debug',
+                        formatted="Master wrong: expecting {}, got {}({})".format(self.master, update.message.from_user.username, update.message.from_user.id))
                     continue
                 else:
                     if not re.match(r'^[0-9]+$', "{}".format(self.config['master'])): # master was not numeric...
                         self.config['master'] = update.message.chat_id
                         idx = (i for i,v in enumerate(self.bot.event_manager._handlers) if type(v) is TelegramHandler).next()
-                        self.bot.event_manager._handlers[idx] = TelegramHandler(self.tbot,self.config['master'], self.config.get('alert_catch'))
-                        
-
-
+                        self.bot.event_manager._handlers[idx] = TelegramHandler(self.tbot, self.master, self.config.get('alert_catch'))
                 if update.message.text == "/info":
                     stats = self._get_player_stats()
                     if stats:
@@ -73,17 +68,26 @@ class TelegramTask(BaseTask):
                                 "_Poke Stop Visits:_ "+str(stats["poke_stop_visits"])+" ("+str(ps_day)+" _last 24h_)",
                                 "_KM Walked:_ "+str(stats["km_walked"])
                             )
-                            self.tbot.sendMessage(chat_id=update.message.chat_id, parse_mode='Markdown', text="\n".join(res))
-                            self.tbot.send_location(chat_id=update.message.chat_id, latitude=self.bot.api._position_lat, longitude=self.bot.api._position_lng)
+                            self.send_message(chat_id=update.message.chat_id, parse_mode='Markdown', text="\n".join(res))
+                            self.send_location(chat_id=update.message.chat_id, latitude=self.bot.api._position_lat, longitude=self.bot.api._position_lng)
                     else:
-                        self.tbot.sendMessage(chat_id=update.message.chat_id, parse_mode='Markdown', text="Stats not loaded yet\n")
+                        self.send_message(chat_id=update.message.chat_id, parse_mode='Markdown', text="Stats not loaded yet\n")
                 elif update.message.text == "/start" or update.message.text == "/help":
                     res = (
                         "Commands: ",
                         "/info - info about bot"
                     )
-                    self.tbot.sendMessage(chat_id=update.message.chat_id, parse_mode='Markdown', text="\n".join(res))
-
+                    self.send_message(chat_id=update.message.chat_id, parse_mode='Markdown', text="\n".join(res))
+    def send_message(self, chat_id, parse_mode, text):
+        try:
+            self.tbot.sendMessage(chat_id, parse_mode, text)
+        except telegram.error.NetworkError:
+            pass
+    def send_location(self, chat_id, latitude, longitude):
+        try:
+            self.tbot.send_location(chat_id, latitude, longitude)
+        except telegram.error.NetworkError:
+            pass
     def _get_player_stats(self):
         """
         Helper method parsing the bot inventory object and returning the player stats object.
@@ -91,18 +95,18 @@ class TelegramTask(BaseTask):
         :rtype: dict
         """
         web_inventory = os.path.join(_base_dir, "web", "inventory-%s.json" % self.bot.config.username)
-        
+
         try:
             with open(web_inventory, "r") as infile:
                 json_inventory = json.load(infile)
         except ValueError:
             # Unable to read json from web inventory
-            # File may be corrupt. Create a new one.  
-            self.bot.logger.info('[x] Error while opening inventory file for read: %s' % e)
+            # File may be corrupt. Create a new one.
+            self.bot.logger.info('[x] Error while opening inventory file for read: %s' % ValueError)
             json_inventory = []
         except:
-            raise FileIOException("Unexpected error reading from {}".web_inventory)
-            
+            raise FileIOException("Unexpected error reading from {}".format(web_inventory))
+
         return next((x["inventory_item_data"]["player_stats"]
                      for x in json_inventory
                      if x.get("inventory_item_data", {}).get("player_stats", {})),
