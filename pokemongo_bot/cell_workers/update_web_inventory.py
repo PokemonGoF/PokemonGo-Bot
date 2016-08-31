755be7e6c493db4dafd08db905b98858d5cc08e7
Julien Lavergne <julien.lavergne@hotmail.com>
2016-08-31 12:26:21 +0800

    Add upgrade_level parameter Fix a mistake in upgrade cost Some refactoring Top default to 0 = all

--- pokemongo_bot/cell_workers/update_web_inventory.py
+++ pokemongo_bot/cell_workers/update_web_inventory.py
@@ -1,15 +1,12 @@
-import json
-import os
-from pokemongo_bot.base_task import BaseTask
 from pokemongo_bot import inventory
-from pokemongo_bot.base_dir import _base_dir
+from pokemongo_bot.base_task import BaseTask
+from pokemongo_bot.worker_result import WorkerResult
 
 
 class UpdateWebInventory(BaseTask):
     SUPPORTED_TASK_API_VERSION = 1
 
-    def initialize(self):
-        pass
-
     def work(self):
         inventory.update_web_inventory()
+
+        return WorkerResult.SUCCESS
