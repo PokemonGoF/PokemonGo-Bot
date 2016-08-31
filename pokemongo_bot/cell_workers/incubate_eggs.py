685a151c036d11f5090ae7718f257767134fe0e0
DeXtroTip <DeXtroTip@users.noreply.github.com>
2016-08-31 20:38:00 +0100

    Little code updates  * Added return of WorkerResult that depends on egg hatching  * Fixed eggs and incubators list not being updates after last one used  * Now using class Pokemon from inventory

--- pokemongo_bot/cell_workers/incubate_eggs.py
+++ pokemongo_bot/cell_workers/incubate_eggs.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from pokemongo_bot import inventory
 from pokemongo_bot.human_behaviour import sleep
 from pokemongo_bot.base_task import BaseTask
+from pokemongo_bot.worker_result import WorkerResult
 
 
 class IncubateEggs(BaseTask):
@@ -18,7 +19,6 @@ class IncubateEggs(BaseTask):
         self.eggs = []
         self.km_walked = 0
         self.hatching_animation_delay = 4.20
-        self.max_iv = 45.0
 
         self._process_config()
 
@@ -26,7 +26,6 @@ class IncubateEggs(BaseTask):
         self.infinite_longer_eggs_first = self.config.get("infinite_longer_eggs_first", False)
         self.breakable_longer_eggs_first = self.config.get("breakable_longer_eggs_first", True)
         self.min_interval = self.config.get('min_interval', 120)
-
         self.breakable_incubator = self.config.get("breakable", [2,5,10])
         self.infinite_incubator = self.config.get("infinite", [2,5,10])
 
@@ -34,20 +33,19 @@ class IncubateEggs(BaseTask):
         try:
             self._check_inventory()
         except:
-            return
+            return WorkerResult.ERROR
 
-        should_print = self._should_print()
+        worker = WorkerResult.SUCCESS
 
         if self.used_incubators and IncubateEggs.last_km_walked != self.km_walked:
             self.used_incubators.sort(key=lambda x: x.get("km"))
             km_left = self.used_incubators[0]['km']-self.km_walked
             if km_left <= 0:
-                self._hatch_eggs()
-                should_print = False
+                worker = self._hatch_eggs()
             else:
                 self.bot.metrics.next_hatching_km(km_left)
 
-        if should_print:
+        if self._should_print():
             self._print_eggs()
             self._compute_next_update()
 
@@ -59,12 +57,13 @@ class IncubateEggs(BaseTask):
             eggs = self._filter_sort_eggs(self.infinite_incubator,
                     self.infinite_longer_eggs_first)
             self._apply_incubators(eggs, self.ready_infinite_incubators)
-
         if self.ready_breakable_incubators:
             # get available eggs
             eggs = self._filter_sort_eggs(self.breakable_incubator,
                     self.breakable_longer_eggs_first)
             self._apply_incubators(eggs, self.ready_breakable_incubators)
+
+        return worker
 
 
     def _filter_sort_eggs(self, allowed, sorting):
@@ -75,7 +74,6 @@ class IncubateEggs(BaseTask):
 
 
     def _apply_incubators(self, available_eggs, available_incubators):
-
         for incubator in available_incubators:
             for egg in available_eggs:
                 if egg["used"] or egg["km"] == -1:
@@ -166,33 +164,22 @@ class IncubateEggs(BaseTask):
                         "used": False
                     })
                 elif 'is_egg' not in pokemon and pokemon['id'] in lookup_ids:
-                    pokemon.update({
-                        "iv": [
-                            pokemon.get('individual_attack', 0),
-                            pokemon.get('individual_defense', 0),
-                            pokemon.get('individual_stamina', 0)
-                        ]})
                     matched_pokemon.append(pokemon)
                 continue
             if "player_stats" in inv_data:
                 self.km_walked = inv_data.get("player_stats", {}).get("km_walked", 0)
-        if temp_used_incubators:
-            self.used_incubators = temp_used_incubators
-        if temp_ready_breakable_incubators:
-            self.ready_breakable_incubators = temp_ready_breakable_incubators
-        if temp_ready_infinite_incubators:
-            self.ready_infinite_incubators = temp_ready_infinite_incubators
-        if temp_eggs:
-            self.eggs = temp_eggs
+        self.used_incubators = temp_used_incubators
+        self.ready_breakable_incubators = temp_ready_breakable_incubators
+        self.ready_infinite_incubators = temp_ready_infinite_incubators
+        self.eggs = temp_eggs
         return matched_pokemon
 
     def _hatch_eggs(self):
         response_dict = self.bot.api.get_hatched_eggs()
-        log_color = 'green'
         try:
             result = reduce(dict.__getitem__, ["responses", "GET_HATCHED_EGGS"], response_dict)
         except KeyError:
-            return
+            return WorkerResult.ERROR
         pokemon_ids = []
         if 'pokemon_id' in result:
             pokemon_ids = [id for id in result['pokemon_id']]
@@ -202,15 +189,10 @@ class IncubateEggs(BaseTask):
         sleep(self.hatching_animation_delay)
         try:
             pokemon_data = self._check_inventory(pokemon_ids)
-            for pokemon in pokemon_data:
-                # pokemon ids seem to be offset by one
-                if pokemon['pokemon_id']!=-1:
-                    pokemon['name'] = self.bot.pokemon_list[(pokemon.get('pokemon_id')-1)]['Name']
-                    #remove as egg and add as pokemon
-                    inventory.pokemons().remove(pokemon['id'])
-                    inventory.pokemons().add(inventory.Pokemon(pokemon))
-                else:
-                    pokemon['name'] = "error"
+            pokemon_list = [inventory.Pokemon(p) for p in pokemon_data]
+            for pokemon in pokemon_list:
+                inventory.pokemons().remove(pokemon.unique_id)
+                inventory.pokemons().add(pokemon)
         except:
             pokemon_data = [{"name":"error", "cp":"error", "iv":"error"}]
         if not pokemon_ids or not pokemon_data or pokemon_data[0]['name'] == "error":
@@ -225,25 +207,25 @@ class IncubateEggs(BaseTask):
                     'candy': 'error',
                 }
             )
-            return
-        for i in range(len(pokemon_data)):
-            msg = "Egg hatched with a {pokemon} (CP {cp} - IV {iv}), {exp} exp, {stardust} stardust and {candy} candies."
-            self.bot.metrics.hatched_eggs(1)
+            return WorkerResult.ERROR
+        for i in range(len(pokemon_list)):
+            msg = "Egg hatched with a {pokemon} (CP {cp} - NCP {ncp} - IV {iv_ads} {iv_pct}), {exp} exp, {stardust} stardust and {candy} candies."
             self.emit_event(
                 'egg_hatched',
                 formatted=msg,
                 data={
-                    'pokemon': pokemon_data[i]['name'],
-                    'cp': pokemon_data[i]['cp'],
-                    'iv': "{} {}".format(
-                        "/".join(map(str, pokemon_data[i]['iv'])),
-                        round(sum(pokemon_data[i]['iv'])/self.max_iv, 2)
-                    ),
+                    'pokemon': pokemon_list[i].name,
+                    'cp': pokemon_list[i].cp,
+                    'ncp': round(pokemon_list[i].cp_percent, 2),
+                    'iv_ads': pokemon_list[i].iv_display(),
+                    'iv_pct': pokemon_list[i].iv,
                     'exp': xp[i],
                     'stardust': stardust[i],
                     'candy': candy[i],
                 }
             )
+        self.bot.metrics.hatched_eggs(len(pokemon_list))
+        return WorkerResult.SUCCESS
 
     def _print_eggs(self):
         if not self.used_incubators:
@@ -254,14 +236,14 @@ class IncubateEggs(BaseTask):
         eggs = ['{:.2f}/{} km'.format(e['km_needed']-e['km']+self.km_walked, e['km_needed']) for e in self.used_incubators]
 
         self.emit_event(
-                    'next_egg_incubates',
-                    formatted='Eggs incubating: [{eggs}] (Eggs left: {eggs_left}, Incubating: {eggs_inc})',
-                    data={
-                        'eggs_left': len(self.eggs),
-                        'eggs_inc': len(self.used_incubators),
-                        'eggs': ', '.join(eggs)
-                    }
-                )
+            'next_egg_incubates',
+            formatted='Eggs incubating: [{eggs}] (Eggs left: {eggs_left}, Incubating: {eggs_inc})',
+            data={
+                'eggs_left': len(self.eggs),
+                'eggs_inc': len(self.used_incubators),
+                'eggs': ', '.join(eggs)
+            }
+        )
 
     def _should_print(self):
         """
