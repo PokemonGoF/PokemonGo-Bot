fbd3c27028773ed8da4849107921cb0b75f83c23
Matt J Madsen <mattjmadsen@gmail.com>
2016-09-03 00:04:57 -0500

    Remove alt delta

--- pokemongo_bot/walkers/step_walker.py
+++ pokemongo_bot/walkers/step_walker.py
@@ -83,7 +83,7 @@ class StepWalker(object):
 
         cLat = self.initLat + scaledDLat + random_lat_long_delta()
         cLng = self.initLng + scaledDLng + random_lat_long_delta()
-        cAlt = self.initAlt + self.unitAlt + random_alt_delta()
+        cAlt = self.initAlt + self.unitAlt
 
         self.api.set_position(cLat, cLng, cAlt)
         self.bot.event_manager.emit(
