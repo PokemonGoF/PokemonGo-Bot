85d3ef394530891073d291e0b7aec63368617335
Matt J Madsen <mattjmadsen@gmail.com>
2016-09-03 08:28:17 -0500

    Revert

--- pokemongo_bot/human_behaviour.py
+++ pokemongo_bot/human_behaviour.py
@@ -24,9 +24,6 @@ def random_lat_long_delta():
     # Return random value from [-.000025, .000025]. Since 364,000 feet is equivalent to one degree of latitude, this
     # should be 364,000 * .000025 = 9.1. So it returns between [-9.1, 9.1]
     return ((random() * 0.00001) - 0.000005) * 5
-    
-def random_lat_long_delta2(bearing):
-    return ((random() * 0.00001) - 0.000005) * 5, ((random() * 0.00001) - 0.000005) * 5
 
 def random_alt_delta():
     # Return random value from [-0.2, 0.2]. Altitude is measured in meters. A delta of 0.4 could for example 
