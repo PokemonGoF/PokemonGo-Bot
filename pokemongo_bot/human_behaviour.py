28bb383c8c3f19c5243a812bd3eaafca381486fd
Matt J Madsen <mattjmadsen@gmail.com>
2016-09-03 05:50:09 -0500

    Add bearing parameter

--- pokemongo_bot/human_behaviour.py
+++ pokemongo_bot/human_behaviour.py
@@ -20,7 +20,7 @@ def action_delay(low, high):
     time.sleep(shortNum)
 
 
-def random_lat_long_delta():
+def random_lat_long_delta(bearing):
     # Return random value from [-.000025, .000025]. Since 364,000 feet is equivalent to one degree of latitude, this
     # should be 364,000 * .000025 = 9.1. So it returns between [-9.1, 9.1]
     return ((random() * 0.00001) - 0.000005) * 5
