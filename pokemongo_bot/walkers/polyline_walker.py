834c5a97344112bdc9551c48e0ebe1956bb7ed44
Matt J Madsen <mattjmadsen@gmail.com>
2016-09-03 02:13:23 -0500

    Update polyline_walker.py

--- pokemongo_bot/walkers/polyline_walker.py
+++ pokemongo_bot/walkers/polyline_walker.py
@@ -11,45 +11,10 @@ class PolylineWalker(StepWalker):
         self.speed = self.bot.config.walk_min
         self.dest_lat, self.dest_lng = dest_lat, dest_lng
         self.actual_pos = tuple(self.bot.position[:2])
-        self.actual_alt = self.bot.position[-1]
-        
-        self.dist = distance(
-            self.bot.position[0],
-            self.bot.position[1],
-            dest_lat,
-            dest_lng
-        
-        # we don't need this if I call StepWalker's stepper right?
-        #super(PolylineWalker, self).__init__(self.bot, self.pol_lat, self.pol_lon,
-        #                                     self.pol_alt, fixed_speed=True)
-
-    def step(self):
         self.polyline = PolylineObjectHandler.cached_polyline(self.actual_pos,
                                                               (self.dest_lat, self.dest_lng),
-                                                              self.speed)
-        
-        if self.dist < 10: # 10m, add config? set it at constants?
-            return True
-        
-        self.polyline_walker.unpause()
-        sleep(1)
-        self.polyline_walker.pause()
-        
+                                                              self.speed, google_map_api_key=self.bot.config.gmapkey)
         self.pol_lat, self.pol_lon = self.polyline.get_pos()
-        self.pol_alt = self.polyline.get_alt() or uniform(self.bot.config.alt_min, self.bot.config.alt_max)
-        
-        step_walker = StepWalker(
-            self.bot,
-            self.pol_lat,
-            self.pol_lon.
-            self.pol_alt,
-            self.speed
-        )
-
-        step_walker.step():
-        
-        self.actual_pos (self.pol_lat, self.pol_lon) # might be a case this instance is reused in the future...
-        
-        self.bot.heartbeat()
-        
-        return False
+        self.pol_alt = self.polyline.get_alt() or self.alt
+        super(PolylineWalker, self).__init__(self.bot, self.pol_lat, self.pol_lon,
+                                             self.pol_alt, fixed_speed=True)
