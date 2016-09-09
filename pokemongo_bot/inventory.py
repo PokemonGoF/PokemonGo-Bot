7d78d637e61747cb652210a0dc623e5696c94733
Gobberwart <gobberwart@gmail.com>
2016-09-10 07:44:19 +1000

    Bandaid for crash on inventory.add

--- pokemongo_bot/inventory.py
+++ pokemongo_bot/inventory.py
@@ -416,9 +416,12 @@ class Pokemons(_BaseInventoryComponent):
 
     def add(self, pokemon):
         if pokemon.unique_id <= 0:
-            raise ValueError("Can't add a pokemon without id")
+            self.bot.logger.debug("Can't add a pokemon without id")
+            return
         if pokemon.unique_id in self._data:
-            raise ValueError("Pokemon already present in the inventory")
+            self.bot.logger.debug("Pokemon already present in the inventory")
+            return
+            
         self._data[pokemon.unique_id] = pokemon
 
     def remove(self, pokemon_unique_id):
