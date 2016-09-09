5a0b68632c5fe3703dce17a8e95edea19527db8b
Gobberwart <gobberwart@gmail.com>
2016-09-09 12:48:28 +1000

    Moved common events to chat_handler

--- pokemongo_bot/event_handlers/chat_handler.py
+++ pokemongo_bot/event_handlers/chat_handler.py
@@ -5,8 +5,9 @@ from pokemongo_bot import inventory
 DEBUG_ON = False
 
 class ChatHandler:
-    def __init__(self, bot):
+    def __init__(self, bot, pokemons):
         self.bot = bot
+        self.pokemons = pokemons
 
     def get_player_stats(self):
         stats = inventory.player().player_stats
@@ -29,6 +30,41 @@ class ChatHandler:
         else:
             return("Stats not loaded yet\n")
             
+    def get_event(self, event, formatted_msg, data):
+        if event == 'level_up':
+            msg = "level up ({})".format(data["current_level"])
+        elif event == 'pokemon_caught':
+            if isinstance(self.pokemons, list): # alert_catch is a plain list
+                if data["pokemon"] in self.pokemons or "all" in self.pokemons:
+                    msg = "Caught {} CP: {}, IV: {}".format(data["pokemon"], data["cp"], data["iv"])
+                else:
+                    return "Catch error 1"
+            else: # alert_catch is a dict
+                if data["pokemon"] in self.pokemons:
+                    trigger = self.pokemons[data["pokemon"]]
+                elif "all" in self.pokemons:
+                    trigger = self.pokemons["all"]
+                else:
+                    return
+                if (not "operator" in trigger or trigger["operator"] == "and") and data["cp"] >= trigger["cp"] and data["iv"] >= trigger["iv"] or ("operator" in trigger and trigger["operator"] == "or" and (data["cp"] >= trigger["cp"] or data["iv"] >= trigger["iv"])):
+                    msg = "Caught {} CP: {}, IV: {}".format(data["pokemon"], data["cp"], data["iv"])
+                else:
+                    return "Catch error 2"
+        elif event == 'egg_hatched':
+            msg = "Egg hatched with a {} CP: {}, IV: {} {}".format(data["name"], data["cp"], data["iv_ads"], data["iv_pct"])
+        elif event == 'bot_sleep':
+            msg = "I am too tired, I will take a sleep till {}.".format(data["wake"])
+        elif event == 'catch_limit':
+            msg = "*You have reached your daily catch limit, quitting.*"
+        elif event == 'spin_limit':
+            msg = "*You have reached your daily spin limit, quitting.*"
+        else:
+            return formatted_msg
+
+        return msg
+
+
+            
     def display_events(self, update):
         cmd = update.message.text.split(" ", 1)
         if len(cmd) > 1:
@@ -38,7 +74,7 @@ class ChatHandler:
             # no filter
             event_filter = ".*"
         events = filter(lambda k: re.match(event_filter, k), self.bot.event_manager._registered_events.keys())
-        self.sendMessage(chat_id=update.message.chat_id, parse_mode='HTML', text=("\n".join(events)))
+        return "\n".join(events)
 
     def showtop(self, chatid, num, order):
         if not num.isnumeric():
@@ -52,17 +88,12 @@ class ChatHandler:
         pkmns = sorted(inventory.pokemons().all(), key=lambda p: getattr(p, order), reverse=True)[:num]
 
         outMsg = "\n".join(["{} CP:{} IV:{} ID:{} Candy:{}".format(p.name, p.cp, p.iv, p.unique_id, inventory.candies().get(p.pokemon_id).quantity) for p in pkmns])
-        self.sendMessage(chat_id=chatid, parse_mode='HTML', text=outMsg)
-
-        return
+        return outMsg
 
     def evolve(self, chatid, uid):
         # TODO: here comes evolve logic (later)
-        self.sendMessage(chat_id=chatid, parse_mode='HTML', text="Evolve logic not implemented yet")
-        return
+        return "Evolve logic not implemented yet"
 
     def upgrade(self, chatid, uid):
         # TODO: here comes upgrade logic (later)
-        self.sendMessage(chat_id=chatid, parse_mode='HTML', text="Upgrade logic not implemented yet")
-        return
-
+        return "Upgrade logic not implemented yet"
