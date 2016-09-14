d812755702fa19fc853ffbbcef27c6da60454bc1
javajohnhub <joedw1978@gmail.com>
2016-09-13 20:04:27 -0400

    travis error update_live_inventory

--- pokemongo_bot/cell_workers/__init__.py
+++ pokemongo_bot/cell_workers/__init__.py
@@ -21,7 +21,7 @@ from update_web_inventory import UpdateWebInventory
 from catch_pokemon import CatchPokemon
 from complete_tutorial import CompleteTutorial
 from random_pause import RandomPause
-from update_live_inventory import UpdateLiveInventory
+#from update_live_inventory import UpdateLiveInventory
 from random_alive_pause import RandomAlivePause
 from show_best_pokemon import ShowBestPokemon
 from telegram_task import TelegramTask
