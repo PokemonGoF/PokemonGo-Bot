e1f3d602b6570466ddf34bcdc6a94ca82943476c
javajohnhub <joedw1978@gmail.com>
2016-09-22 12:24:32 -0400

    fix pokemon caught being displayed in telegram

--- pokemongo_bot/event_handlers/chat_handler.py
+++ pokemongo_bot/event_handlers/chat_handler.py
@@ -38,28 +38,28 @@ class ChatHandler:
         msg = None
         if event == 'level_up':
             msg = "level up ({})".format(data["current_level"])
-        elif event == 'pokemon_caught':
+        if event == 'pokemon_caught':
             trigger = None
-            if data["pokemon"] in self.pokemons:		
-                trigger = self.pokemons[data["pokemon"]]		
-            elif "all" in self.pokemons:		
+            if data["pokemon"] in self.pokemons:
+                trigger = self.pokemons[data["pokemon"]]
+            elif "all" in self.pokemons:
                 trigger = self.pokemons["all"]
             if trigger:
                 if ((not "operator" in trigger or trigger["operator"] == "and") and data["cp"] >= trigger["cp"] and data["iv"] >= trigger["iv"]) or \
                         ("operator" in trigger and trigger["operator"] == "or" and (data["cp"] >= trigger["cp"] or data["iv"] >= trigger["iv"])):
                     msg = "Caught {} CP: {}, IV: {}".format(data["pokemon"], data["cp"], data["iv"])
-        elif event == 'egg_hatched':
+        if event == 'egg_hatched':
             msg = "Egg hatched with a {} CP: {}, IV: {} (A/D/S {})".format(data["name"], data["cp"], data["iv_pct"], data["iv_ads"])
-        elif event == 'bot_sleep':
+        if event == 'bot_sleep':
             msg = "I am too tired, I will take a sleep till {}.".format(data["wake"])
-        elif event == 'catch_limit':
+        if event == 'catch_limit':
             msg = "*You have reached your daily catch limit, quitting.*"
-        elif event == 'spin_limit':
+        if event == 'spin_limit':
             msg = "*You have reached your daily spin limit, quitting.*"
-        else:
+        if msg == None:
             return formatted_msg
-
-        return msg
+        else:
+            return msg
 
     def get_events(self, update):
         cmd = update.message.text.split(" ", 1)
